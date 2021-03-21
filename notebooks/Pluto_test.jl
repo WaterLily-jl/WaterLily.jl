2d8c142ffb87307a249ab32d4d78d2b2f8efed65
Gabriel Weymouth <drweymouth@google.com>
2021-03-21 21:06:42 +0000

    Update Pluto example

--- notebooks/Pluto_test.jl
+++ notebooks/Pluto_test.jl
@@ -4,32 +4,46 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ bc0bf1e8-8a36-11eb-3cc2-ff079d49df5b
-using WaterLily
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
 
-# ╔═╡ 419fdcec-8a38-11eb-2f8c-fdede732476d
+# ╔═╡ bc0bf1e8-8a36-11eb-3cc2-ff079d49df5b
+using WaterLily, PlutoUI
+
+# ╔═╡ c9eca20a-8a82-11eb-1e65-c735d7bee4c0
 include("../examples/TwoD_plots.jl");
 
 # ╔═╡ 7ac5d8a4-8a43-11eb-2ef0-e3c0aa4737b0
-radius=24
+radius=16
 
 # ╔═╡ 3e4f5c2a-8a3a-11eb-0465-433b036a7086
-body = AutoBody((x,t)-> √sum(abs2, x .- 2radius) - radius);
+body = AutoBody((x,t)-> √sum(abs2, x .- 4radius) - radius);
 
 # ╔═╡ 1dcb3cb0-8a44-11eb-18cd-cf841f9222b1
-sim = Simulation((6radius+2,4radius+2),[1.,0.],radius; body, ν=radius/1000);
+sim = Simulation((12radius+2,8radius+2),[1.,0.],radius; body, ν=radius/200);
+
+# ╔═╡ 6612b570-8a80-11eb-20e3-69d56c5ff765
+@bind tick PlutoUI.Clock(0.001)
 
 # ╔═╡ b8e4b74e-8a3a-11eb-27a0-87eea85497d9
-begin 
-	sim_step!(sim,sim_time(sim)+1)
+begin
+	tick
+	sim_step!(sim,sim_time(sim)+0.25)
 	@inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
-	flood(sim.flow.σ)
+	flood(sim.flow.σ,clims=(-10,10))
 end
 
 # ╔═╡ Cell order:
 # ╠═bc0bf1e8-8a36-11eb-3cc2-ff079d49df5b
-# ╠═419fdcec-8a38-11eb-2f8c-fdede732476d
+# ╠═c9eca20a-8a82-11eb-1e65-c735d7bee4c0
 # ╠═7ac5d8a4-8a43-11eb-2ef0-e3c0aa4737b0
 # ╠═3e4f5c2a-8a3a-11eb-0465-433b036a7086
 # ╠═1dcb3cb0-8a44-11eb-18cd-cf841f9222b1
+# ╠═6612b570-8a80-11eb-20e3-69d56c5ff765
 # ╠═b8e4b74e-8a3a-11eb-27a0-87eea85497d9
