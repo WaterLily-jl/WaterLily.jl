4c6ee953416a14c4433eb5ba6e7860884c8f54f6
Gabriel Weymouth <drweymouth@google.com>
2021-03-29 10:14:02 +0100

    Update flowcases.jl

--- benchmark/flowcases.jl
+++ benchmark/flowcases.jl
@@ -1,14 +1,23 @@
 using WaterLily
 using BenchmarkTools
+using LinearAlgebra: norm2
+using Profile
 
-function benchmark_step(;radius=8,Re=250)
-    body = AutoBody((x,t)-> √sum(abs2, x .- 2radius) - radius)
-    gen_sim = ()->Simulation((6radius+2,4radius+2),[1.,0.],radius; body, ν=radius/Re)
+radius = 8; Re = 250
+body = AutoBody((x,t)-> norm2(x .- 2radius) - radius)
+gen_sim = ()->Simulation((6radius+2,4radius+2),[1.,0.],radius; body, ν=radius/Re)
+sim = gen_sim()
+
+function benchmark_step()
+    @benchmark mom_step!($sim.flow,$sim.pois)
+end
+
+function benchmark_sim()
     @benchmark sim_step!(sim,10) setup=(sim=$gen_sim())
 end
 
 function benchmark_measure(func::Function=measure!)
     flow = Flow((66,66),[1.,0.])
     @benchmark $func($flow,AutoBody(
-            (x,t)-> √sum(abs2, x .- 2radius) - radius)) setup=(radius=5*(1+rand()))
+            (x,t)-> norm2(x .- 2radius) - radius)) setup=(radius=5*(1+rand()))
 end
