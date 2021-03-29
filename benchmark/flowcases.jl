2dbdab90e025d1dfc9ad9aaedb412a36fff8f5d6
weymouth <gabriel.weymouth@gmail.com>
2021-03-29 12:22:24 +0100

    Benchmarked slices branch
    The speed up is around 10% for a 2D simulation with a body, and 33% for 
    the 3D TGV! (This reflects the larger importance of the pressure solve 
    for 2D obstructed flows.)
    
    Master:
    > sim = body_sim(); benchmark_step()
    BenchmarkTools.Trial: 
      memory estimate:  0 bytes
      allocs estimate:  0
      --------------
      minimum time:     217.301 μs (0.00% GC)
      median time:      226.501 μs (0.00% GC)
      mean time:        237.085 μs (0.00% GC)
      maximum time:     974.500 μs (0.00% GC)
      --------------
      samples:          10000
      evals/sample:     1
    
    > sim = vort_sim(); benchmark_step()
    BenchmarkTools.Trial: 
      memory estimate:  0 bytes
      allocs estimate:  0
      --------------
      minimum time:     7.224 ms (0.00% GC)
      median time:      10.110 ms (0.00% GC)
      mean time:        10.012 ms (0.00% GC)
      maximum time:     14.005 ms (0.00% GC)
      --------------
      samples:          500
      evals/sample:     1
    
    slices:
    > sim = body_sim(); benchmark_step()
    BenchmarkTools.Trial: 
      memory estimate:  0 bytes
      allocs estimate:  0
      --------------
      minimum time:     196.701 μs (0.00% GC)
      median time:      204.600 μs (0.00% GC)
      mean time:        211.477 μs (0.00% GC)
      maximum time:     776.900 μs (0.00% GC)
      --------------
      samples:          10000
      evals/sample:     1
    
    > sim = vort_sim(); benchmark_step()
    BenchmarkTools.Trial: 
      memory estimate:  0 bytes
      allocs estimate:  0
      --------------
      minimum time:     6.857 ms (0.00% GC)
      median time:      7.634 ms (0.00% GC)
      mean time:        7.716 ms (0.00% GC)
      maximum time:     11.578 ms (0.00% GC)
      --------------
      samples:          648
      evals/sample:     1

--- benchmark/flowcases.jl
+++ benchmark/flowcases.jl
@@ -5,19 +5,17 @@ using Profile
 
 radius = 8; Re = 250
 body = AutoBody((x,t)-> norm2(x .- 2radius) - radius)
-gen_sim = ()->Simulation((6radius+2,4radius+2),[1.,0.],radius; body, ν=radius/Re)
-sim = gen_sim()
+body_sim = ()->Simulation((6radius+2,4radius+2),[1.,0.],radius; body, ν=radius/Re)
+
+L = 2^5
+function uλ(i,vx)
+    x,y,z = @. (vx-1.5)*π/L              # scaled coordinates
+    i==1 && return -sin(x)*cos(y)*cos(z) # u_x
+    i==2 && return  cos(x)*sin(y)*cos(z) # u_y
+    return 0.                            # u_z
+end
+vort_sim = ()->Simulation((L+2,L+2,L+2),zeros(3),L;uλ,ν=L/Re,U=1.)
 
 function benchmark_step()
     @benchmark mom_step!($sim.flow,$sim.pois)
-end
-
-function benchmark_sim()
-    @benchmark sim_step!(sim,10) setup=(sim=$gen_sim())
-end
-
-function benchmark_measure(func::Function=measure!)
-    flow = Flow((66,66),[1.,0.])
-    @benchmark $func($flow,AutoBody(
-            (x,t)-> norm2(x .- 2radius) - radius)) setup=(radius=5*(1+rand()))
 end
