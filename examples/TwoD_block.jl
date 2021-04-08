390f65c13d89880ac45c766b6f1cb40b1b9a37ee
Gabriel Weymouth <drweymouth@google.com>
2021-04-08 14:55:11 +0100

    Added tilt to TwoD_block example
    Added rotation angle α = amp*cos(t*U/L) to demonstrate a non-trivial set 
    of kinematics.

--- examples/TwoD_block.jl
+++ examples/TwoD_block.jl
@@ -2,19 +2,20 @@ using WaterLily
 using LinearAlgebra: norm2
 include("TwoD_plots.jl")
 
-function block(L=2^5;Re=250,U=0.25)
+function block(L=2^5;Re=250,U=0.25,amp=0)
     # Set viscosity
     ν=U*L/Re
     @show L,ν
 
     # Create dynamic block geometry
-    function sdf(x,t)
-        x[2] -= clamp(x[2],-L/2,L/2)
-        norm2(x)-1
+    sdf(x,t) = (x[2] -= clamp(x[2],-L/2,L/2); norm2(x)-1)
+    function map(x,t)
+        α = amp*cos(t*U/L)
+        [cos(α) sin(α); -sin(α) cos(α)] * (x.-[3L+L*sin(t*U/L),4L])
     end
-    map(x,t) = x.-[3L+L*sin(t*U/L),2L]
-    body=AutoBody(sdf,map)
+    body = AutoBody(sdf,map)
 
-    Simulation((6L+2,4L+2),zeros(2),L;U,ν,body)
+    Simulation((6L+2,6L+2),zeros(2),L;U,ν,body)
 end
 # sim_gif!(block();duration=4π,step=π/16,remeasure=true)
+# sim_gif!(block(amp=π/4);duration=8π,step=π/16,remeasure=true,μbody=true,cfill=:Blues,legend=false,border=:none)
