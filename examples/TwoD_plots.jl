6522b59a18f6d3ba6e50528fe851cc1cb207fabf
Gabriel Weymouth <drweymouth@google.com>
2021-04-08 14:52:27 +0100

    Added μbody_plot!
    μbody_plot! adds the body contour from the μ field to a plot. Added this 
    as a flag for sim_gif! as well as leting you pass keyword arguments 
    through to flood().

--- examples/TwoD_plots.jl
+++ examples/TwoD_plots.jl
@@ -15,15 +15,22 @@ function flood(f::Array;shift=(0.,0.),cfill=:RdBu_11,clims=(),kv...)
 end
 
 addbody(x,y;c=:black) = Plots.plot!(Shape(x,y), c=c, legend=false)
+function μbody_plot!(flow::Flow{m};levels=[0.5],lines=:black) where {m}
+    @inside flow.σ[I] = sum(WaterLily.ϕ(i,CartesianIndex(I,i),flow.μ₀) for i ∈ 1:m)/m
+    BC!(flow.σ)
+    contour!(flow.σ';levels,lines)
+end
 
-function sim_gif!(sim;duration=1,step=0.1,verbose=true,remeasure=false)
+function sim_gif!(sim;duration=1,step=0.1,verbose=true,
+                  remeasure=false,μbody=false,size=(700,600),kv...)
     t₀ = round(sim_time(sim))
     t = range(t₀,t₀+duration;step)
-    gr(show=false)
+    gr(show=false;size)
     @time @gif for tᵢ in t
         sim_step!(sim,tᵢ;remeasure)
         @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
-        flood(sim.flow.σ,shift=(-0.5,-0.5),clims=(-5,5))
+        flood(sim.flow.σ;shift=(-0.5,-0.5),clims=(-5,5),kv...)
+        μbody && μbody_plot!(sim.flow)
         verbose && println("tU/L=",round(tᵢ,digits=4),
             ", Δt=",round(sim.flow.Δt[end],digits=3))
     end
