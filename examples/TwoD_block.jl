b3c9910ffcaaf4814a715a89e371e95382821845
weymouth <gabriel.weymouth@gmail.com>
2021-04-15 16:03:06 +0100

    Use StaticArrays in block example
    It is really important to use SVector/Smatric in more complex 
    `sdf`,`map` functions. The new benchmarck code went from 23 seconds and 
    123 M allocations to 3.0 seconds and 193 k allocations!

--- examples/TwoD_block.jl
+++ examples/TwoD_block.jl
@@ -1,5 +1,6 @@
 using WaterLily
 using LinearAlgebra: norm2
+using StaticArrays
 include("TwoD_plots.jl")
 
 function block(L=2^5;Re=250,U=1,amp=0,ϵ=0.5,thk=2ϵ+√2)
@@ -8,14 +9,18 @@ function block(L=2^5;Re=250,U=1,amp=0,ϵ=0.5,thk=2ϵ+√2)
     @show L,ν
 
     # Create dynamic block geometry
-    sdf(x,t) = (x[2] -= clamp(x[2],-L/2,L/2); norm2(x)-thk/2)
+    function sdf(x,t)
+        y = x .- SVector(0.,clamp(x[2],-L/2,L/2))
+        norm2(y)-thk/2
+    end
     function map(x,t)
-        α = amp*cos(t*U/L)
-        [cos(α) sin(α); -sin(α) cos(α)] * (x.-[3L+L*sin(t*U/L)+0.01,4L])
+        α = amp*cos(t*U/L); R = @SMatrix [cos(α) sin(α); -sin(α) cos(α)]
+        R * (x.-SVector(3L+L*sin(t*U/L)+0.01,4L))
     end
     body = AutoBody(sdf,map)
 
     Simulation((6L+2,6L+2),zeros(2),L;U,ν,body,ϵ)
 end
+# test() = @time sim_step!(block(),π,remeasure=true)
 # sim_gif!(block();duration=4π,step=π/16,remeasure=true)
 # sim_gif!(block(amp=π/4);duration=8π,step=π/16,remeasure=true,μbody=true,cfill=:Blues,legend=false,border=:none)
