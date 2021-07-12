7642c537072e4873221ff0a9316de527fda5bf7f
Stefan Zitz <s.zitz@fz-juelich.de>
2021-07-12 12:22:37 +0200

    Now the docs from @autodocs should deploy

--- docs/make.jl
+++ docs/make.jl
@@ -3,14 +3,14 @@ using Documenter, WaterLily
 makedocs(
     modules = [WaterLily],
     repo="https://github.com/gabrielweymouth/WaterLily.jl",
-    sitename="WaterLily",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", nothing) == "true",
         canonical="https:/Zitzeronion.github.io/WaterLily.jl",
         assets=String[],
+    ),
     authors = "Gabriel Weymouth",
     sitename = "WaterLily.jl",
-    pages = Any["index.md"]
+    pages = ["index.md"]
     # strict = true,
     # clean = true,
     # checkdocs = :exports,
--- docs/src/index.md
+++ docs/src/index.md
@@ -10,8 +10,3 @@ Modules = [WaterLily]
 
 ```@index
 ```
-
-<!--- This generates just a collection of all documentation you have written.
-Therefore it collects all docstrings and pushes them into a html file.
-Not super nice but a starting for the documentation.
-For this to work I think you need github pages, which you can enable in the github options of the project. --->
