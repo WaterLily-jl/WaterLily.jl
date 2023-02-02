bb57db360b0f02c22be497ddba4c1a9229a60294
Blagneaux <118433296+Blagneaux@users.noreply.github.com>
2023-02-02 11:27:24 +0100

    add of addBody function
    This function takes as input a list of SVectors containing the sdf and the map for every body to add to the simulation. The output is an Autobody to use as input for the global simulation.

--- src/WaterLily.jl
+++ src/WaterLily.jl
@@ -1,14 +1,5 @@
 module WaterLily
 
-_nthread = Threads.nthreads()
-if _nthread==1
-    @warn "WaterLily.jl is running on a single thread.\n
-Launch Julia with multiple threads to enable multithreaded capabilities:\n
-    \$julia -t auto $PROGRAM_FILE"
-else
-    print("WaterLily.jl is running on ", _nthread, " thread(s)\n")
-end
-
 include("util.jl")
 export L₂,BC!,@inside,inside,δ,apply!,loc
 
@@ -105,5 +96,66 @@ function measure!(sim::Simulation,t=time(sim))
     update!(sim.pois,sim.flow.μ₀)
 end
 
-export Simulation,sim_step!,sim_time,measure!
+@fastmath kern₀(d) = 0.5+0.5d+0.5sin(π*d)/π
+μ₀(d,ϵ) = kern₀(clamp(d/ϵ,-1,1))
+
+function offset(Bodies, L)
+	"""Creates a different offset for each of the bodies so that when the general map is computed, there is no problem of
+	body spontaneous generation. This offset is then substracted when the individual map is applied where it needs to be thus
+	hiding its existence."""
+	
+    sdfList = [ (x,t) -> offsetSdf(x,t,i) for i in 1:length(Bodies)]
+    mapList = [ (x,t) -> offsetMap(x,t,i) for i in 1:length(Bodies)]
+
+	function offsetSdf(x,t, i)
+		xc = x + [0.,(-1)^i * 100*i * L]
+		return Bodies[i][1](xc,t)
+	end
+
+	function offsetMap(x,t, i)
+		xc = x - [0.,(-1)^i * 100*i * L]
+		return Bodies[i][2](xc,t)
+	end
+
+	return (sdfList, mapList)
+end
+
+function addBody(Bodies::Array{SVector{Function, Function}}, L=100)
+	"""addBody(Bodies::Array{SVector{Function, Function}}, L=100)
+    
+        Bodies: array of SVector(sdf, map) for each of the independent bodies to add to the window.
+        L: carateristic dimension of the largest body, to create a great enough offset to delete the generetion of undesired body.
+
+        
+    The default distance between two independent bodies is set to 100L. It impacts both their placement to not disturbe the other maps,
+	in the function 'offset', and the selection of the second closest body to a given point in the function 'min_excluding_i'.
+	The coefficients are computed to determine where each map should be used, therefore creating a global map that impacts the 
+	whole simulation window.
+
+	The output can directly be used as the body argument in the Simulation function provided by WaterLily."""
+
+	sdfList, mapList = offset(Bodies, L)
+
+	function min_excluding_i(sdfL, mapL, i, x, t)
+		min_val = 100L
+		for j in eachindex(sdfL)
+			if j != i 
+				val = sdfL[j](mapL[j](x,t),t)
+				if val <= min_val
+					min_val = val
+				end
+			end
+		end
+		return min_val
+	end
+
+	coef = [(x,t) -> μ₀(min_excluding_i(sdfList, mapList, i, x, t) - sdfList[i](mapList[i](x,t),t),1) for i in range(1, length(sdfList))]
+
+	sdf(x,t) = minimum([sdfX(x,t) for sdfX in sdfList])
+	map(x,t) = sum([mapList[i](x,t)*coef[i](x,t) for i in range(1, length(mapList))])
+
+	return AutoBody(sdf, map)
+end
+
+export Simulation,sim_step!,sim_time,measure!, addBody
 end # module
