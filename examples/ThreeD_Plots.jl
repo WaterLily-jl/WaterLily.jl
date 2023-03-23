64e16b6325005ca589c1fbf671bc103dd86fb0bc
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-03-23 07:58:05 +0530

    Fix up ThreeD_Plots.jl to work with new Makie

--- examples/ThreeD_Plots.jl
+++ examples/ThreeD_Plots.jl
@@ -1,15 +1,19 @@
 using GLMakie
 
-function volume_video!(sim,data_func;name="file.mp4",duration=1,step=0.1)
+function volume_video!(sim,data_func;name="file.mp4",duration=1,step=0.1; framerate = 30, compression = 20)
     # Set up viz data and figure
-    dat = Node(data_func(sim))
-    fig = volume(dat,colorrange=(π,4π),algorithm=:absorption,
-                    backgroundcolor = RGBf0(0.9, 0.9, 0.9))
+    dat = Observable(data_func(sim))
+    fig, ax, plt = volume(
+        dat,
+        colorrange=(π,4π),
+        algorithm=:absorption,
+        axis = (backgroundcolor = RGBf0(0.9, 0.9, 0.9),)
+    )
 
     # Run simulation and update figure data
     t₀ = round(sim_time(sim))
     t = range(t₀,t₀+duration;step)
-    record(fig,name,t) do tᵢ
+    record(fig, name, t; framerate, compression) do tᵢ
         sim_step!(sim,tᵢ)
         dat[] = data_func(sim)
         println("simulation ",round(Int,(tᵢ-t₀)/duration*100),"% complete")
@@ -17,16 +21,16 @@ function volume_video!(sim,data_func;name="file.mp4",duration=1,step=0.1)
     return sim,fig
 end
 
-function contour_video!(sim,data_func,geom_func;name="file.mp4",duration=1,step=0.1)
+function contour_video!(sim,data_func,geom_func;name="file.mp4",duration=1,step=0.1; framerate = 30, compression = 20)
     # Set up viz data and figure
-    fig = contour(geom_func(sim),levels=[0.5])
-    dat = Node(data_func(sim))
-    contour!(dat,levels=[-7,7],colormap=:balance,alpha=0.2,colorrange=[-7,7])
+    fig, ax, plt1 = contour(geom_func(sim), levels=[0.5])
+    dat = Observable(data_func(sim))
+    plt2 = contour!(ax, dat, levels=[-7,7], colormap=:balance, alpha=0.2, colorrange=[-7,7])
 
     # Run simulation and update figure data
     t₀ = round(sim_time(sim))
     t = range(t₀,t₀+duration;step)
-    record(fig,name,t) do tᵢ
+    record(fig, name, t; framerate, compression) do tᵢ
         sim_step!(sim,tᵢ)
         dat[] = data_func(sim)
         println("simulation ",round(Int,(tᵢ-t₀)/duration*100),"% complete")
