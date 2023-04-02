6e652b915074eb9480bc351aacb32f79007fcc92
Gabe Weymouth <gweymouth@tudelft.nl>
2023-04-02 15:50:21 +0200

    Extend @loop macro to deal with structs
    Added a sketch of the Flow struct (using a mapping to convert to CuArrays, which might be a nice way to do it).
    
    The previous version of the @loop macro failed when given an expression with composite values like
    ```
    @loop flow.div[I] = div_operator(I,flow.u)
    ```
    which is a problem since there are TONS of these in the code-base.
    Problem 1: `:(flow.u)` is an Expr not a Symbol, so `grab!()` broke this up into `:flow, :u`, which isn't what we want to pass to the function. This is solved by simply aborting the recursion when `head==:.`.
    Problem 2: `:(flow.u)` is not a valid function argument, meaning you can not write
    ```
    function f(flow.div,flow.u) # invalid arguments
       I = @index()
       flow.div[I] = div_operator(I,flow.u)
    end
    ```
    Instead you need to replace these with the composite value itself *including in the expression* ie
    ```
    function f(div,u) # rename arguments
       I = @index()
       div[I] = div_operator(I,u) # replace arguments in expression
    end
    f(flow.div,flow.u) # call with full composite values
    ```
    This was pretty easy, with some help from a discourse post. Now it's running and tested to match the hand-written kernel.

--- CUDAEnv/macros.jl
+++ CUDAEnv/macros.jl
@@ -2,12 +2,18 @@ using KernelAbstractions,CUDA,CUDA.CUDAKernels,Adapt,OffsetArrays
 using OffsetArrays: Origin
 CUDA.allowscalar(false)
 
-N = (4+2,3+2); D = length(N)
-Nd = (N...,D); Od = (zeros(Int,D)...,1)
-u = adapt(CuArray,Origin(Od)(rand(Float32,Nd)));
-div = adapt(CuArray,Origin(0)(zeros(Float32,N)));
-div2 = copy(div);
- 
+struct Flow{Scal,Vect}
+    u :: Vect
+    σ :: Scal
+    function Flow(N::NTuple{D}; f=identity, T = Float32) where D
+        Nd = (N...,D); Od = (zeros(Int,D)...,1)
+        u = rand(T,Nd)|>Origin(Od)|>f
+        σ = zeros(T,N)|>Origin(0)|>f
+        new{typeof(σ),typeof(u)}(u,σ)
+    end
+end
+Base.size(a::Flow) = size(a.σ).-2
+
 @inline δ(i,I::CartesianIndex{m}) where{m} = CartesianIndex(ntuple(j -> j==i ? 1 : 0, m))
 @inline ∂(a,I::CartesianIndex{m},u::AbstractArray{T,n}) where {T,n,m} = @inbounds u[I+δ(a,I),a]-u[I,a]
 @fastmath @inline function div_operator(I::CartesianIndex{m},u) where {m} 
@@ -23,36 +29,36 @@ end
     I = @index(Global, Cartesian)
     div[I] = div_operator(I,u)
 end
-div_hand(div,u) = div_kernel(CUDABackend(),64)(div,u,ndrange=size(div).-2)
-div_hand(div,u) # works
+div_hand!(flow::Flow) = div_kernel(KernelAbstractions.get_backend(flow.σ),64)(flow.σ,flow.u,ndrange=size(flow.σ).-2)
+
+flow = Flow((6,6));
+div_hand!(flow) # works
 
 # Automate writing the kernel from the expression
 macro loop(args...)
     ex,_,itr = args
     _,I,R = itr.args; sym = []
-    grab!(sym,ex)
+    grab!(sym,ex)     # get arguments and replace composites in `ex`
     setdiff!(sym,[I]) # don't want to pass I as an argument
-    @gensym kernel
     return quote
-        @kernel function $kernel($(sym...))
+        @kernel function f($(rep.(sym)...)) # replace composite arguments
             $I = @index(Global, Cartesian)
             $ex
-        end;
-        $kernel(CUDABackend(),64)($(sym...),ndrange=$R)
+        end
+        f(KernelAbstractions.get_backend($(sym[1])),64)($(sym...),ndrange=$R)
+        return nothing
     end |> esc
 end
-# Grab all the arguments from an expression
 function grab!(sym,ex::Expr)
-    start = ex.head==:(call) ? 2 : 1
-    for args in ex.args[start:end]
-        grab!(sym,args)
-    end
+    ex.head == :. && return union!(sym,[ex])    # keep composited names without recursion
+    start = ex.head==:(call) ? 2 : 1            # don't grab function names
+    foreach(a->grab!(sym,a),ex.args[start:end]) # recurse
+    ex.args .= rep.(ex.args)                    # replace composite names with value
 end
-@inline grab!(sym,ex::Symbol) = union!(sym,[ex])
-@inline grab!(sym,ex) = nothing
-
-# Test
-@macroexpand1 @loop a[I,i] = coeff[I,i]*grad(i,I,p) over I ∈ size(p).-2
+grab!(sym,ex::Symbol) = union!(sym,[ex])        # keep symbol names
+grab!(sym,ex) = nothing
+rep(ex) = ex
+rep(ex::Expr) = ex.head == :. ? Symbol(ex.args[2].value) : ex
 
 # Automate the range for assignment loops
 macro inside(ex)
@@ -63,21 +69,20 @@ macro inside(ex)
         @loop $ex over $I ∈ size($a).-2
     end |> esc
 end
+@macroexpand @inside flow.σ[I] = div_operator(I,flow.u)
+divergence!(flow) = @inside flow.σ[I] = div_operator(I,flow.u)
 
 # Test against the hand-written kernel
-@macroexpand1 @inside div2[I] = div_operator(I,u)
-@inside div2[I] = div_operator(I,u)
-div.parent ≈ div2.parent
+div = copy(flow.σ); fill!(flow.σ,0); 
+divergence!(flow) # works
+flow.σ == div # true
 
 # Benchmark against the hand-written kernel
 using BenchmarkTools
-N = (2^10+2,2^10+2); D = length(N)
-Nd = (N...,D); Od = (zeros(Int,D)...,1)
-u = adapt(CuArray,Origin(Od)(rand(Float32,Nd)));
-div = adapt(CuArray,Origin(0)(zeros(Float32,N)));
+flowCPU = Flow((2^10+2,2^10+2));
+flowGPU = Flow((2^10+2,2^10+2),f=a->adapt(CuArray,a));
 
-@btime div_hand($div,$u) # 2.389 μs (52 allocations: 2.80 KiB)
-# div_macro also creates the function - so it's not a fair comparison
-div_macro(div,u) = @inside div[I] = div_operator(I,u)
-@btime div_macro($div,$u) # 2.700 μs (62 allocations: 2.94 KiB) 
-# still close
+@btime div_hand!($flowCPU) # 111.000 μs (200 allocations: 17.94 KiB)
+@btime div_hand!($flowGPU) # 2.178 μs (51 allocations: 2.64 KiB)
+@btime divergence!($flowCPU); # 122.200 μs (209 allocations: 18.22 KiB) 
+@btime divergence!($flowGPU); # 2.700 μs (62 allocations: 2.94 KiB) 
