9eac5ac5503ad25d20a9875ebdeeda1e87382c60
Gabe Weymouth <gweymouth@tudelft.nl>
2023-04-03 21:38:55 +0200

    Starting to integrate changes and test
    I have commented out most of the WaterLily.jl and test\runtests.jl files. The plan is to roll out the changes to the code base one file at a time, and add the tests as we go.
    
    Currently tested most of the serial code in util, including OA and the new version of inside().

--- src/WaterLily.jl
+++ src/WaterLily.jl
@@ -1,100 +1,100 @@
 module WaterLily
 
 include("util.jl")
-export L₂,BC!,@inside,inside,δ,apply!,loc
+export L₂,inside,δ,OA,loc#,apply!,BC!,@inside
 
-include("Poisson.jl")
-export AbstractPoisson,Poisson,solver!,mult
+# include("Poisson.jl")
+# export AbstractPoisson,Poisson,solver!,mult
 
 # include("MultiLevelPoisson.jl")
 # export MultiLevelPoisson,solver!,mult
 
-include("Flow.jl")
-export Flow,mom_step!
+# include("Flow.jl")
+# export Flow,mom_step!
 
-include("Body.jl")
-export AbstractBody
+# include("Body.jl")
+# export AbstractBody
 
-include("AutoBody.jl")
-export AutoBody,measure!,measure,+,-
+# include("AutoBody.jl")
+# export AutoBody,measure!,measure,+,-
 
-include("Metrics.jl")
-using LinearAlgebra: norm2
+# include("Metrics.jl")
+# using LinearAlgebra: norm2
 
-"""
-    Simulation(dims::Tuple, u_BC::Vector, L::Number;
-               U=norm2(u_BC), Δt=0.25, ν=0., ϵ = 1,
-               uλ::Function=(i,x)->u_BC[i],
-               body::AbstractBody=NoBody())
+# """
+#     Simulation(dims::Tuple, u_BC::Vector, L::Number;
+#                U=norm2(u_BC), Δt=0.25, ν=0., ϵ = 1,
+#                uλ::Function=(i,x)->u_BC[i],
+#                body::AbstractBody=NoBody())
 
-Constructor for a WaterLily.jl simulation:
+# Constructor for a WaterLily.jl simulation:
 
-    `dims`: Simulation domain dimensions.
-    `u_BC`: Simulation domain velocity boundary conditions, `u_BC[i]=uᵢ, i=1,2...`.
-    `L`: Simulation length scale.
-    `U`: Simulation velocity scale.
-    `ϵ`: BDIM kernel width.
-    `Δt`: Initial time step.
-    `ν`: Scaled viscosity (`Re=UL/ν`)
-    `uλ`: Function to generate the initial velocity field.
-    `body`: Immersed geometry
+#     `dims`: Simulation domain dimensions.
+#     `u_BC`: Simulation domain velocity boundary conditions, `u_BC[i]=uᵢ, i=1,2...`.
+#     `L`: Simulation length scale.
+#     `U`: Simulation velocity scale.
+#     `ϵ`: BDIM kernel width.
+#     `Δt`: Initial time step.
+#     `ν`: Scaled viscosity (`Re=UL/ν`)
+#     `uλ`: Function to generate the initial velocity field.
+#     `body`: Immersed geometry
 
-See files in `examples` folder for examples.
-"""
-struct Simulation
-    U :: Number # velocity scale
-    L :: Number # length scale
-    ϵ :: Number # kernel width
-    flow :: Flow
-    body :: AbstractBody
-    pois :: AbstractPoisson
-    function Simulation(dims::Tuple, u_BC::Vector, L::Number;
-                        Δt=0.25, ν=0., U=norm2(u_BC), ϵ = 1,
-                        uλ::Function=(i,x)->u_BC[i],
-                        body::AbstractBody=NoBody(),T=Float64)
-        flow = Flow(dims,u_BC;uλ,Δt,ν,T)
-        measure!(flow,body;ϵ)
-        new(U,L,ϵ,flow,body,MultiLevelPoisson(flow.μ₀))
-    end
-end
+# See files in `examples` folder for examples.
+# """
+# struct Simulation
+#     U :: Number # velocity scale
+#     L :: Number # length scale
+#     ϵ :: Number # kernel width
+#     flow :: Flow
+#     body :: AbstractBody
+#     pois :: AbstractPoisson
+#     function Simulation(dims::Tuple, u_BC::Vector, L::Number;
+#                         Δt=0.25, ν=0., U=norm2(u_BC), ϵ = 1,
+#                         uλ::Function=(i,x)->u_BC[i],
+#                         body::AbstractBody=NoBody(),T=Float64)
+#         flow = Flow(dims,u_BC;uλ,Δt,ν,T)
+#         measure!(flow,body;ϵ)
+#         new(U,L,ϵ,flow,body,MultiLevelPoisson(flow.μ₀))
+#     end
+# end
 
-time(sim::Simulation) = sum(sim.flow.Δt[1:end-1])
-"""
-    sim_time(sim::Simulation)
+# time(sim::Simulation) = sum(sim.flow.Δt[1:end-1])
+# """
+#     sim_time(sim::Simulation)
 
-Return the current dimensionless time of the simulation `tU/L`
-where `t=sum(Δt)`, and `U`,`L` are the simulation velocity and length
-scales.
-"""
-sim_time(sim::Simulation) = time(sim)*sim.U/sim.L
+# Return the current dimensionless time of the simulation `tU/L`
+# where `t=sum(Δt)`, and `U`,`L` are the simulation velocity and length
+# scales.
+# """
+# sim_time(sim::Simulation) = time(sim)*sim.U/sim.L
 
-"""
-    sim_step!(sim::Simulation,t_end;verbose=false)
+# """
+#     sim_step!(sim::Simulation,t_end;verbose=false)
 
-Integrate the simulation `sim` up to dimensionless time `t_end`.
-If `verbose=true` the time `tU/L` and adaptive time step `Δt` are
-printed every time step.
-"""
-function sim_step!(sim::Simulation,t_end;verbose=false,remeasure=false)
-    t = time(sim)
-    while t < t_end*sim.L/sim.U
-        remeasure && measure!(sim,t)
-        mom_step!(sim.flow,sim.pois) # evolve Flow
-        t += sim.flow.Δt[end]
-        verbose && println("tU/L=",round(t*sim.U/sim.L,digits=4),
-            ", Δt=",round(sim.flow.Δt[end],digits=3))
-    end
-end
+# Integrate the simulation `sim` up to dimensionless time `t_end`.
+# If `verbose=true` the time `tU/L` and adaptive time step `Δt` are
+# printed every time step.
+# """
+# function sim_step!(sim::Simulation,t_end;verbose=false,remeasure=false)
+#     t = time(sim)
+#     while t < t_end*sim.L/sim.U
+#         remeasure && measure!(sim,t)
+#         mom_step!(sim.flow,sim.pois) # evolve Flow
+#         t += sim.flow.Δt[end]
+#         verbose && println("tU/L=",round(t*sim.U/sim.L,digits=4),
+#             ", Δt=",round(sim.flow.Δt[end],digits=3))
+#     end
+# end
 
-"""
-    measure!(sim::Simulation,t=time(sim))
+# """
+#     measure!(sim::Simulation,t=time(sim))
 
-Measure a dynamic `body` to update the `flow` and `pois` coefficients.
-"""
-function measure!(sim::Simulation,t=time(sim))
-    measure!(sim.flow,sim.body;t,ϵ=sim.ϵ)
-    update!(sim.pois,sim.flow.μ₀)
-end
+# Measure a dynamic `body` to update the `flow` and `pois` coefficients.
+# """
+# function measure!(sim::Simulation,t=time(sim))
+#     measure!(sim.flow,sim.body;t,ϵ=sim.ϵ)
+#     update!(sim.pois,sim.flow.μ₀)
+# end
 
-export Simulation,sim_step!,sim_time,measure!
+# export Simulation,sim_step!,sim_time,measure!
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,143 +1,139 @@
 using WaterLily
 using Test
-using PerformanceTestTools
-using JLD2
-using Polyester
-
-@testset "multithreaded equivalence" begin
-    N_multithread = 2
-    PerformanceTestTools.@include_foreach(
-        "tests_using_threads.jl",
-        [["JULIA_NUM_THREADS" => "1"]],
-    )
-    PerformanceTestTools.@include_foreach(
-        "tests_using_threads.jl",
-        [["JULIA_NUM_THREADS" => string(N_multithread)]],
-    )
-    filebase = string(tempdir(), "/testing_using_threads#")
-    sim1_u = load(string(filebase, 1, "_u.jld2"))["data"]
-    sim2_u = load(string(filebase, N_multithread, "_u.jld2"))["data"]
-    @test maximum(broadcast(abs, sim1_u-sim2_u)) < 1e-3
-end
 
 @testset "util.jl" begin
-    @test L₂(2ones(4,4)) == 16
-    a = Float64[i+j+k  for i ∈ 1:4, j ∈ 1:4, k ∈ 1:2]
-    BC!(a,[0. 0.])
-    @test a == cat([0. 0. 0. 0.
-                    0. 0. 0. 0.
-                    6. 6. 7. 7.
-                    0. 0. 0. 0.],
-                   [0. 0. 7. 0.
-                    0. 0. 7. 0.
-                    0. 0. 8. 0.
-                    0. 0. 8. 0.],dims=3)
-    u = zeros(4,4,2); apply!((i,x)->x[i],u)
-    @test [u[i,j,1].-(i-0.5) for i in 1:4, j in 1:4]==zeros(4,4)
+    using WaterLily: OA
+    I = CartesianIndex(1,2,3,4)
+    @test I+δ(3,I) == CartesianIndex(1,2,4,4)
+
+    p = Float64[i+j  for i ∈ 1:4, j ∈ 1:5]
+    @test inside(p) == CartesianIndices((2:3,2:4))
+    @show p
+    @test L₂(p) == 187
+
+    p = p|>OA()
+    @test inside(p) == CartesianIndices((1:2,1:3))
+    @test L₂(p) == 187 # unchanged!
+
+    u = Float64[i+j+k  for i ∈ 1:4, j ∈ 1:4, k ∈ 1:2] |> OA(2)
+    @test first.(axes(u)) == (0,0,1)
+
     using StaticArrays
     @test loc(3,CartesianIndex(3,4,5)) == SVector(3,4,4.5)
     I = CartesianIndex(rand(2:10,3)...)
     @test loc(0,I) == SVector(I.I...)
+
+    # BC!(a,[0. 0.])
+    # @test a == cat([0. 0. 0. 0.
+    #                 0. 0. 0. 0.
+    #                 6. 6. 7. 7.
+    #                 0. 0. 0. 0.],
+    #                [0. 0. 7. 0.
+    #                 0. 0. 7. 0.
+    #                 0. 0. 8. 0.
+    #                 0. 0. 8. 0.],dims=3)
+    # u = zeros(4,4,2); apply!((i,x)->x[i],u)
+    # @test [u[i,j,1].-(i-0.5) for i in 1:4, j in 1:4]==zeros(4,4)
 end
 
-function Poisson_test_2D(f,n)
-    c = ones(n+2,n+2,2); BC!(c,[0. 0.])
-    p = f(c)
-    soln = Float64[ i for i ∈ 1:n+2, j ∈ 1:n+2]
-    b = mult(p,soln)
-    x = zeros(n+2,n+2)
-    solver!(x,p,b)
-    x .-= (x[2,2]-soln[2,2])
-    return L₂(x.-soln)/L₂(soln)
-end
-function Poisson_test_3D(f,n)
-    c = ones(n+2,n+2,n+2,3); BC!(c,[0. 0. 0.])
-    p = f(c)
-    soln = Float64[ i for i ∈ 1:n+2, j ∈ 1:n+2, k ∈ 1:n+2]
-    b = mult(p,soln)
-    x = zeros(n+2,n+2,n+2)
-    solver!(x,p,b,tol=1e-5)
-    x .-= (x[2,2,2]-soln[2,2,2])
-    return L₂(x.-soln)/L₂(soln)
-end
+# function Poisson_test_2D(f,n)
+#     c = ones(n+2,n+2,2); BC!(c,[0. 0.])
+#     p = f(c)
+#     soln = Float64[ i for i ∈ 1:n+2, j ∈ 1:n+2]
+#     b = mult(p,soln)
+#     x = zeros(n+2,n+2)
+#     solver!(x,p,b)
+#     x .-= (x[2,2]-soln[2,2])
+#     return L₂(x.-soln)/L₂(soln)
+# end
+# function Poisson_test_3D(f,n)
+#     c = ones(n+2,n+2,n+2,3); BC!(c,[0. 0. 0.])
+#     p = f(c)
+#     soln = Float64[ i for i ∈ 1:n+2, j ∈ 1:n+2, k ∈ 1:n+2]
+#     b = mult(p,soln)
+#     x = zeros(n+2,n+2,n+2)
+#     solver!(x,p,b,tol=1e-5)
+#     x .-= (x[2,2,2]-soln[2,2,2])
+#     return L₂(x.-soln)/L₂(soln)
+# end
 
-@testset "Poisson.jl" begin
-    @test Poisson_test_2D(Poisson,2^6) < 1e-5
-    @test Poisson_test_3D(Poisson,2^4) < 1e-5
-end
-@testset "MultiLevelPoisson.jl" begin
-    I = CartesianIndex(4,3,2)
-    @test all(WaterLily.down(J)==I for J ∈ WaterLily.up(I))
-    @test_throws AssertionError("MultiLevelPoisson requires size=a2ⁿ, where a<31, n>2") Poisson_test_2D(MultiLevelPoisson,67)
-    @test_throws AssertionError("MultiLevelPoisson requires size=a2ⁿ, where a<31, n>2") Poisson_test_3D(MultiLevelPoisson,3^4)
-    @test Poisson_test_2D(MultiLevelPoisson,2^6) < 1e-5
-    @test Poisson_test_3D(MultiLevelPoisson,2^4) < 1e-5
-end
+# @testset "Poisson.jl" begin
+#     @test Poisson_test_2D(Poisson,2^6) < 1e-5
+#     @test Poisson_test_3D(Poisson,2^4) < 1e-5
+# end
+# @testset "MultiLevelPoisson.jl" begin
+#     I = CartesianIndex(4,3,2)
+#     @test all(WaterLily.down(J)==I for J ∈ WaterLily.up(I))
+#     @test_throws AssertionError("MultiLevelPoisson requires size=a2ⁿ, where a<31, n>2") Poisson_test_2D(MultiLevelPoisson,67)
+#     @test_throws AssertionError("MultiLevelPoisson requires size=a2ⁿ, where a<31, n>2") Poisson_test_3D(MultiLevelPoisson,3^4)
+#     @test Poisson_test_2D(MultiLevelPoisson,2^6) < 1e-5
+#     @test Poisson_test_3D(MultiLevelPoisson,2^4) < 1e-5
+# end
 
-@testset "Body.jl" begin
-    @test WaterLily.μ₀(3,6)==WaterLily.μ₀(0.5,1)
-    @test WaterLily.μ₀(0,1)==0.5
-    @test WaterLily.μ₁(0,2)==2*(1/4-1/π^2)
-end
+# @testset "Body.jl" begin
+#     @test WaterLily.μ₀(3,6)==WaterLily.μ₀(0.5,1)
+#     @test WaterLily.μ₀(0,1)==0.5
+#     @test WaterLily.μ₁(0,2)==2*(1/4-1/π^2)
+# end
 
-@testset "AutoBody.jl" begin
-    using LinearAlgebra: norm2
+# @testset "AutoBody.jl" begin
+#     using LinearAlgebra: norm2
 
-    # test AutoDiff in 2D and 3D
-    body1 = AutoBody((x,t)->norm2(x)-2-t)
-    @test all(measure(body1,[√2.,√2.],0.).≈(0,[√.5,√.5],[0.,0.]))
-    @test all(measure(body1,[2.,0.,0.],1.).≈(-1.,[1.,0.,0.],[0.,0.,0.]))
-    body2 = AutoBody((x,t)->norm2(x)-2,(x,t)->x.+t^2)
-    @test all(measure(body2,[√2.,√2.],0.).≈(0,[√.5,√.5],[0.,0.]))
-    @test all(measure(body2,[1.,-1.,-1.],1.).≈(0.,[1.,0.,0.],[-2.,-2.,-2.]))
+#     # test AutoDiff in 2D and 3D
+#     body1 = AutoBody((x,t)->norm2(x)-2-t)
+#     @test all(measure(body1,[√2.,√2.],0.).≈(0,[√.5,√.5],[0.,0.]))
+#     @test all(measure(body1,[2.,0.,0.],1.).≈(-1.,[1.,0.,0.],[0.,0.,0.]))
+#     body2 = AutoBody((x,t)->norm2(x)-2,(x,t)->x.+t^2)
+#     @test all(measure(body2,[√2.,√2.],0.).≈(0,[√.5,√.5],[0.,0.]))
+#     @test all(measure(body2,[1.,-1.,-1.],1.).≈(0.,[1.,0.,0.],[-2.,-2.,-2.]))
 
-    #test booleans
-    @test all(measure(body1+body2,[-√2.,-√2.],1.).≈(-√2.,[-√.5,-√.5],[-2.,-2.]))
-    @test all(measure(body1-body2,[-√2.,-√2.],1.).≈(√2.,[√.5,√.5],[-2.,-2.]))
+#     #test booleans
+#     @test all(measure(body1+body2,[-√2.,-√2.],1.).≈(-√2.,[-√.5,-√.5],[-2.,-2.]))
+#     @test all(measure(body1-body2,[-√2.,-√2.],1.).≈(√2.,[√.5,√.5],[-2.,-2.]))
 
-    # test fast apply_sdf matches exhaustive sdf
-    dims = (2^5,2^5)
-    sdf(x) = norm2(x.-2^4)-4π
-    a = zeros(dims); WaterLily.apply_sdf!(sdf,a)
-    b = zeros(dims); @inside b[I] = sdf(WaterLily.loc(0,I))
-    @test all(@. clamp(a,-2,2)==clamp(b,-2,2))
-end
+#     # test fast apply_sdf matches exhaustive sdf
+#     dims = (2^5,2^5)
+#     sdf(x) = norm2(x.-2^4)-4π
+#     a = zeros(dims); WaterLily.apply_sdf!(sdf,a)
+#     b = zeros(dims); @inside b[I] = sdf(WaterLily.loc(0,I))
+#     @test all(@. clamp(a,-2,2)==clamp(b,-2,2))
+# end
 
-@testset "Flow.jl" begin
-    # Horizontally moving body
-    using LinearAlgebra: norm2
-    a = Flow((20,20),[1.,0.])
-    center = [10.58,10.65] # worst case - not sure why
-    measure!(a,AutoBody((x,t)->norm2(x.-center)-5,(x,t)->x.-[t,0.]))
-    mom_step!(a,Poisson(a.μ₀))
-    @test sum(abs2,a.u[:,5,1].-1) < 2e-5
+# @testset "Flow.jl" begin
+#     # Horizontally moving body
+#     using LinearAlgebra: norm2
+#     a = Flow((20,20),[1.,0.])
+#     center = [10.58,10.65] # worst case - not sure why
+#     measure!(a,AutoBody((x,t)->norm2(x.-center)-5,(x,t)->x.-[t,0.]))
+#     mom_step!(a,Poisson(a.μ₀))
+#     @test sum(abs2,a.u[:,5,1].-1) < 2e-5
 
-    # Impulsive flow in a box
-    U = [2/3,-1/3]
-    a = Flow((14,10),U)
-    mom_step!(a,MultiLevelPoisson(a.μ₀))
-    @test L₂(a.u[:,:,1].-U[1]) < 2e-5
-    @test L₂(a.u[:,:,2].-U[2]) < 1e-5
-end
+#     # Impulsive flow in a box
+#     U = [2/3,-1/3]
+#     a = Flow((14,10),U)
+#     mom_step!(a,MultiLevelPoisson(a.μ₀))
+#     @test L₂(a.u[:,:,1].-U[1]) < 2e-5
+#     @test L₂(a.u[:,:,2].-U[2]) < 1e-5
+# end
 
-@testset "Metrics.jl" begin
-    I = CartesianIndex(2,3,4)
-    u = zeros(3,4,5,3); apply!((i,x)->x[i]+prod(x),u)
-    @test WaterLily.ke(I,u)==0.5*(26^2+27^2+28^2)
-    @test WaterLily.ke(I,u,[2,3,4])===1.5*24^2
-    @test [WaterLily.∂(i,j,I,u)
-            for i in 1:3, j in 1:3] == [13 8 6; 12 9 6; 12 8 7]
-    @test WaterLily.λ₂(I,u)≈1
-    ω = [8-6,6-12,12-8]
-    @test WaterLily.curl(2,I,u)==ω[2]
-    @test WaterLily.ω(I,u)==ω
-    @test WaterLily.ω_mag(I,u)==sqrt(sum(abs2,ω))
-    @test WaterLily.ω_θ(I,[0,0,1],[2,2,2],u)==-ω[1]
+# @testset "Metrics.jl" begin
+#     I = CartesianIndex(2,3,4)
+#     u = zeros(3,4,5,3); apply!((i,x)->x[i]+prod(x),u)
+#     @test WaterLily.ke(I,u)==0.5*(26^2+27^2+28^2)
+#     @test WaterLily.ke(I,u,[2,3,4])===1.5*24^2
+#     @test [WaterLily.∂(i,j,I,u)
+#             for i in 1:3, j in 1:3] == [13 8 6; 12 9 6; 12 8 7]
+#     @test WaterLily.λ₂(I,u)≈1
+#     ω = [8-6,6-12,12-8]
+#     @test WaterLily.curl(2,I,u)==ω[2]
+#     @test WaterLily.ω(I,u)==ω
+#     @test WaterLily.ω_mag(I,u)==sqrt(sum(abs2,ω))
+#     @test WaterLily.ω_θ(I,[0,0,1],[2,2,2],u)==-ω[1]
 
-    body = AutoBody((x,t)->√sum(abs2,x .- 2^6) - 2^5)
-    p = ones(2^7,2^7)
-    @inside p[I] = sum(I.I[2])
-    @test sum(abs2,WaterLily.∮nds(p,body)/(π*2^10).-(0,1))<1e-6
-    @inside p[I] = cos(atan(reverse(loc(0,I) .- 2^6)...))
-    @test sum(abs2,WaterLily.∮nds(p,body)/(π*2^5).-(1,0))<1e-6
-end
+#     body = AutoBody((x,t)->√sum(abs2,x .- 2^6) - 2^5)
+#     p = ones(2^7,2^7)
+#     @inside p[I] = sum(I.I[2])
+#     @test sum(abs2,WaterLily.∮nds(p,body)/(π*2^10).-(0,1))<1e-6
+#     @inside p[I] = cos(atan(reverse(loc(0,I) .- 2^6)...))
+#     @test sum(abs2,WaterLily.∮nds(p,body)/(π*2^5).-(1,0))<1e-6
+# end
