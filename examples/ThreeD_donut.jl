8290176a06721f27e87987b8a182b19c1422d0f7
Gabe Weymouth <gweymouth@tudelft.nl>
2023-04-17 21:31:51 +0200

    Update ThreeD_donut.jl
    Got the ω_θ computation running on the GPU. Viz loop is now down to 5ms out of 70ms per frame.

--- examples/ThreeD_donut.jl
+++ examples/ThreeD_donut.jl
@@ -29,37 +29,27 @@ begin
     d = sim.flow.σ |> Array;
     fig, ax, plt1 = contour(d[inside(d)], levels=[0.5])
 
-    # CPU arrays for u and viz
-    u = sim.flow.u |> Array
+    # Set up observable and fill it with ω_θ
     dat = d[inside(d)] |> Observable;
-
-    # Compute θ-component of ω on CPU
-    function ω_θ_data!(dat,d,u,sim,center)
-        copyto!(u,sim.flow.u) # copy to CPU container
+    function ω_θ!(dat,d,sim,center)
         dt = sim.L/sim.U
-        @inside d[I] = WaterLily.ω_θ(I,SA[1,0,0],center,u)*dt
-        dat[] = d[inside(d)]  # update Observable
+        @inside sim.flow.σ[I] = WaterLily.ω_θ(I,(1,0,0),center,sim.flow.u)*dt
+        copyto!(d,sim.flow.σ); # copy to CPU
+        dat[] = d[inside(d)];  # update Observable
     end
-    ω_θ_data!(dat,d,u,sim,center)
 
-    # Plot flow contours
+    # Plot ω_θ contours
+    ω_θ!(dat,d,sim,center)
     contour!(dat, levels=[-5,5], colormap=:balance)
     fig
 end
 
 # Loop in time
 for _ in 1:100
-    sim_step!(sim,sim_time(sim)+0.05) # Runs on GPU
-    ω_θ_data!(dat,d,u,sim,center)     # Runs on CPU
+    sim_step!(sim,sim_time(sim)+0.05,remeasure=false)
+    ω_θ!(dat,d,sim,center)
 end
 
 # using BenchmarkTools
-# @btime sim_step!($sim,sim_time($sim)+0.05); # 64.772 ms (103213 allocations: 5.21 MiB)
-# @btime ω_θ_data!($dat,$d,$u,$sim,$center); # 45.077 ms (5769052 allocations: 154.08 MiB) Too slow!
-
-# @btime copyto!($u,$sim.flow.u); # 803.700 μs (1 allocation: 16 bytes)
-# # @btime copyto!($d,$sim.flow.σ);  # 3x faster (not surprising)
-# dt = sim.L/sim.U
-# kern(d,center,u,dt) = @inside d[I] = WaterLily.ω_θ(I,SA[1,0,0],center,u)*dt
-# @btime kern($d,$center,$u,$dt); # 39.492 ms (5766738 allocations: 152.00 MiB) !!! This is terrible !!!
-# @btime $dat[] = $d[inside($d)]; # 2.127 ms (2313 allocations: 2.08 MiB)
+# @btime sim_step!($sim,sim_time($sim)+0.05,remeasure=false); # 64.772 ms (103213 allocations: 5.21 MiB)
+# @btime ω_θ!($dat,$d,$sim,$center); # 5.148 ms (2695 allocations: 2.11 MiB) W00T!
