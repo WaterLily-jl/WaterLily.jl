281e781af58ebfa6a6bafd9197150b6bca3397a3
Gabe Weymouth <gweymouth@tudelft.nl>
2023-04-24 18:22:30 +0200

    Update TwoD_circle

--- examples/TwoD_circle.jl
+++ examples/TwoD_circle.jl
@@ -1,11 +1,9 @@
 using WaterLily
-using LinearAlgebra: norm2
-include("TwoD_plots.jl")
-
-function circle(radius=8;Re=250,n=10,m=6)
-    center, ν = radius*m/2, radius/Re
-    body = AutoBody((x,t)->norm2(x .- center) - radius)
-    Simulation((n*radius+2,m*radius+2), [1.,0.], radius; ν, body)
+function circle(radius=8;Re=250,n=(10,6),U=1)
+    center = radius*n[2]/2
+    body = AutoBody((x,t)->√sum(abs2, x .- center) - radius)
+    Simulation(radius .* n, (U,0), radius; ν=U*radius/Re, body)
 end
 
-# sim_gif!(circle(20);duration=10,step=0.25)
+include("TwoD_plots.jl")
+sim_gif!(circle(20),duration=10,clims=(-5,5.05),plotbody=true)
