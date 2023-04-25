e9505d7ba6eeaf12a5d9cdfc0438e7469bed07ed
Gabe Weymouth <gweymouth@tudelft.nl>
2023-04-25 17:09:30 +0200

    Update ThreeD_Plots.jl

--- examples/ThreeD_Plots.jl
+++ examples/ThreeD_Plots.jl
@@ -1,5 +1,5 @@
 using GLMakie
-function makie_video!(makie_plot,sim,dat,obs_update!;name="file.mp4",duration=1,step=0.1,framerate = 30, compression = 20)
+function makie_video!(makie_plot,sim,dat,obs_update!;remeasure=false,name="file.mp4",duration=1,step=0.1,framerate=30,compression=20)
     # Set up viz data and figure
     obs = obs_update!(dat,sim) |> Observable;
     fig, _, _ = makie_plot(obs)
@@ -8,7 +8,7 @@ function makie_video!(makie_plot,sim,dat,obs_update!;name="file.mp4",duration=1,
     t₀ = round(sim_time(sim))
     t = range(t₀,t₀+duration;step)
     record(fig, name, t; framerate, compression) do tᵢ
-        sim_step!(sim,tᵢ)
+        sim_step!(sim,tᵢ;remeasure)
         obs[] = obs_update!(dat,sim)
         println("simulation ",round(Int,(tᵢ-t₀)/duration*100),"% complete")
     end
