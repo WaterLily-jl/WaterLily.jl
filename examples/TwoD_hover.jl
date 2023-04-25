0b907db909ca51425d17c1a9997720beb1cb9317
Gabe Weymouth <gweymouth@tudelft.nl>
2023-04-25 17:22:10 +0200

    Update TwoD_hover.jl

--- examples/TwoD_hover.jl
+++ examples/TwoD_hover.jl
@@ -3,13 +3,13 @@ using StaticArrays
 function hover(L=2^5;Re=250,U=1,amp=π/4,ϵ=0.5,thk=2ϵ+√2)
     # Line segment SDF
     function sdf(x,t)
-        y = x .- SVector(0.,clamp(x[2],-L/2,L/2))
+        y = x .- SA[0,clamp(x[2],-L/2,L/2)]
         √sum(abs2,y)-thk/2
     end
     # Oscillating motion and rotation
     function map(x,t)
-        α = amp*cos(t*U/L); R = @SMatrix [cos(α) sin(α); -sin(α) cos(α)]
-        R * (x.-SVector(3L-L*sin(t*U/L),4L))
+        α = amp*cos(t*U/L); R = SA[cos(α) sin(α); -sin(α) cos(α)]
+        R * (x - SA[3L-L*sin(t*U/L),4L])
     end
     Simulation((6L,6L),(0,0),L;U,ν=U*L/Re,body=AutoBody(sdf,map),ϵ)
 end
