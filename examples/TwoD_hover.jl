95d1b7c512fd65cf77d1edf2e223b574d0c4660c
Gabe Weymouth <gweymouth@tudelft.nl>
2023-04-25 17:16:58 +0200

    Update TwoD_hover.jl

--- examples/TwoD_hover.jl
+++ examples/TwoD_hover.jl
@@ -1,7 +1,6 @@
 using WaterLily
 using StaticArrays
-
-function block(L=2^5;Re=250,U=1,amp=π/4,ϵ=0.5,thk=2ϵ+√2)
+function hover(L=2^5;Re=250,U=1,amp=π/4,ϵ=0.5,thk=2ϵ+√2)
     # Line segment SDF
     function sdf(x,t)
         y = x .- SVector(0.,clamp(x[2],-L/2,L/2))
@@ -12,12 +11,13 @@ function block(L=2^5;Re=250,U=1,amp=π/4,ϵ=0.5,thk=2ϵ+√2)
         α = amp*cos(t*U/L); R = @SMatrix [cos(α) sin(α); -sin(α) cos(α)]
         R * (x.-SVector(3L-L*sin(t*U/L),4L))
     end
-    Simulation((6L+2,6L+2),zeros(2),L;U,ν=U*L/Re,body=AutoBody(sdf,map),ϵ)
+    Simulation((6L,6L),(0,0),L;U,ν=U*L/Re,body=AutoBody(sdf,map),ϵ)
 end
 
-using BenchmarkTools
-test() = @benchmark sim_step!(sim,π/4,remeasure=true) setup=(sim=block())
-
-# include("TwoD_plots.jl")
-# sim_gif!(block();duration=4π,step=π/16,remeasure=true)
-# sim_gif!(block(amp=π/4);duration=8π,step=π/16,remeasure=true,μbody=true,cfill=:Blues,legend=false,border=:none)
+sim = hover();
+sim_step!(sim,π)
+include("TwoD_plots.jl")
+a = sim.flow.σ;
+@inside a[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
+flood(a[inside(a)],clims=(-5,5.05))
+body_plot!(sim)
