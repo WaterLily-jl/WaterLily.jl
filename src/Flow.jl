3fc2fcf05a64a1f11231c79ea865d2a5f3d3ddb9
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2023-06-24 16:09:54 +0200

    Update Flow.jl

--- src/Flow.jl
+++ src/Flow.jl
@@ -42,14 +42,14 @@ function conv_diff!(r,u,Φ;ν=0.1)
 end
 
 """
-    Flow{D, V, S, F, B, T}
+    Flow{D::Int, T::Float, Sf<:AbstractArray{T,D}, Vf<:AbstractArray{T,D+1}, Tf<:AbstractArray{T,D+2}}
 
 Composite type for a multidimensional immersed boundary flow simulation.
 
 Flow solves the unsteady incompressible [Navier-Stokes equations](https://en.wikipedia.org/wiki/Navier%E2%80%93Stokes_equations) on a Cartesian grid.
 Solid boundaries are modelled using the [Boundary Data Immersion Method](https://eprints.soton.ac.uk/369635/).
-The primary variables are the scalar pressure `p` (an array of dimension `N`)
-and the velocity vector field `u` (an array of dimension `M=N+1`).
+The primary variables are the scalar pressure `p` (an array of dimension `D`)
+and the velocity vector field `u` (an array of dimension `D+1`).
 """
 struct Flow{D, T, Sf<:AbstractArray{T}, Vf<:AbstractArray{T}, Tf<:AbstractArray{T}}
     # Fluid fields
