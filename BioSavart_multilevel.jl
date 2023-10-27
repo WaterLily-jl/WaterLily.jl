6efabd3dfe44073382526e91c187416659549115
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2023-10-27 17:07:55 +0200

    scrap

--- BioSavart_multilevel.jl
+++ BioSavart_multilevel.jl
@@ -2,7 +2,7 @@ using WaterLily,StaticArrays
 
 # 2D Multi-level Biot-Savart functions 
 function MLArray(x)
-    levels = [x]
+    levels = [copy(x)]
     N = size(x)
     while all(N .|> WaterLily.divisible)
         N = @. 1+N÷2
@@ -69,7 +69,26 @@ function biotBC!(u,U,ml)
             end
         end
     end
+    # for i ∈ 1:n
+    #     for s ∈ (1,2,N[i])  # Normal direction using biot-savart
+    #         for I ∈ WaterLily.slice(N,s,i)
+    #             x = WaterLily.loc(i,I,Float32)
+    #             u[I,i] = u_ω(i,x,ml)+U[i]
+    #         end
+    #     end
+    #     j = i%2+1 # Tangent direction using ω=0
+    #     WaterLily.@loop u[I,j] = u[I+δ(i,I),j]-WaterLily.∂(j,CartesianIndex(I+δ(i,I),i),u) over I ∈ WaterLily.slice(N,1,i,3)
+    #     WaterLily.@loop u[I,j] = u[I-δ(i,I),j]+WaterLily.∂(j,CartesianIndex(I-δ(i,I),i),u) over I ∈ WaterLily.slice(N,N[i],i,3)
+    # end
 end
+
+function ω_from_p(I::CartesianIndex,b::AbstractPoisson)
+    du(I,i) = @inbounds(-b.L[I,i]*WaterLily.∂(i,I,b.x))
+    ∂(i,j,I,u) = (u(I+δ(j,I),i)+u(I+δ(j,I)+δ(i,I),i)
+                 -u(I-δ(j,I),i)-u(I-δ(j,I)+δ(i,I),i))/4
+    return WaterLily.permute((j,k)->∂(k,j,I,du),3)
+end
+function resid_update()
 
 # Check reconstruction on lamb dipole
 using SpecialFunctions,ForwardDiff
@@ -97,17 +116,46 @@ begin
 end
 
 # Check pressure solver convergence on circle
+include("examples/TwoD_plots.jl")
 circ(N;D=3N/4,U=1) = Simulation((N, N), (U,0), D; body=AutoBody((x,t)->√sum(abs2,x .- N/2)-D/2))
-begin 
-    sim = circ(128); a = sim.flow; a.u .= 0; ml = MLArray(a.σ);
-    WaterLily.conv_diff!(a.f,a.u⁰,a.σ,ν=a.ν);
-    WaterLily.BDIM!(a);
-    for i in 1:30
-        biotBC!(a.u,a.U,ml);
-        @inside a.σ[I] = WaterLily.div(I,a.u)
-        @show L₂(a.σ),maximum(a.u)
-        L₂(a.σ)<1e-3 && (@show i; break)
-        WaterLily.project!(a,sim.pois;itmx=1);
+sim = circ(128); a = sim.flow; b = sim.pois; a.u .= 0; ml = MLArray(a.σ);
+WaterLily.conv_diff!(a.f,a.u⁰,a.σ,ν=a.ν);
+WaterLily.BDIM!(a);
+BC!(a.u,a.U)
+biotBC!(a.u,a.U,ml);
+flood(ml[1])
+@inside b.z[I] = WaterLily.div(I,a.u)
+flood(b.z)
+solver!(b;itmx=1)
+flood(b.levels[1].r)
+
+function update_resid!(b,ml)
+    ml[1] .= 0;
+    WaterLily.@loop ml[1][I] = ω_from_p(I,b) over I ∈ inside(b.z;buff=2)
+    for l ∈ 2:lastindex(ml)
+        WaterLily.restrict!(ml[l],ml[l-1])
     end
-    @assert abs(maximum(a.u)/2-1)<1e-2
-end
+    N,n = WaterLily.size_u(u)
+    for i ∈ 1:n
+        for s ∈ (1,2,N[i])  # Normal direction using biot-savart
+            for I ∈ WaterLily.slice(N,s,i)
+                x = WaterLily.loc(i,I,Float32)
+                u[I,i] = u_ω(i,x,ml)+U[i]
+            end
+        end
+    end
+end 
+
+# begin 
+#     sim = circ(128); a = sim.flow; a.u .= 0; ml = MLArray(a.σ);
+#     WaterLily.conv_diff!(a.f,a.u⁰,a.σ,ν=a.ν);
+#     WaterLily.BDIM!(a);
+#     for i in 1:30
+#         biotBC!(a.u,a.U,ml);
+#         @inside a.σ[I] = WaterLily.div(I,a.u)
+#         @show L₂(a.σ),maximum(a.u)
+#         L₂(a.σ)<1e-3 && (@show i; break)
+#         WaterLily.project!(a,sim.pois;itmx=1);
+#     end
+#     @assert abs(maximum(a.u)/2-1)<1e-2
+# end
