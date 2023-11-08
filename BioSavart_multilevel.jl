723cec6f7aeedc630b3548b8e86ed150f7f6c5e4
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2023-11-08 15:24:06 +0100

    GPU working, but very slow
    I've updated the code to run on the GPU. Unfortunately, running on the GPU with a 2^12 square domain results in a huge slow-down. Both `biotBC!` and `update_resid!` slow down by almost 10x!

--- BioSavart_multilevel.jl
+++ BioSavart_multilevel.jl
@@ -3,20 +3,20 @@ import WaterLily: @loop,divisible,restrict!,permute,up,inside,slice,size_u
 
 # 2D Multi-level Biot-Savart functions 
 function MLArray(x)
-    levels = [zeros(eltype(x),size(x))]
     N = size(x)
-    while all(N .|> divisible)
+    levels = [N]
+    while all(N .|> divisible) && all(N .> 20)
         N = @. 1+N÷2
-        y = similar(x,N); fill!(y,0)
-        push!(levels,y)
+        push!(levels,N)
     end
-    return levels
+    zeros_like_x(N) = (y = similar(x,N); fill!(y,0); y)
+    return Tuple(zeros_like_x(N) for N in levels)
 end
 ml_restrict!(ml) = for l ∈ 2:lastindex(ml)
     restrict!(ml[l],ml[l-1])
 end
 
-function u_ω(i,x,ml_ω,dis=10)
+@fastmath function u_ω(i,x,ml_ω,dis=10)
     # initialize at bottom level
     ui = zero(eltype(x)); j = i%2+1
     l = lastindex(ml_ω)
@@ -31,7 +31,7 @@ function u_ω(i,x,ml_ω,dis=10)
 
         # get contributions outside Rclose
         for I ∈ R
-            !(I ∈ Rclose) && (ui += ω[I]*biotsavart(r(x,I,dx),j))
+            !(I ∈ Rclose) && (ui += @inbounds(ω[I])*biotsavart(r(x,I,dx),j))
         end
 
         # move "up" one level within Rclose
@@ -43,29 +43,32 @@ function u_ω(i,x,ml_ω,dis=10)
 
     # top level contribution
     for I ∈ R
-        ui += ω[I]*biotsavart(r(x,I,dx),j)
+        ui += @inbounds(ω[I])*biotsavart(r(x,I,dx),j)
     end
     return ui
 end
-biotsavart(r,j) = Float32((3-2j)*r[j]/(2π*r'*r))
-r(x,I,dx) = x-dx*loc(0,I,Float32)
-inR(x,R) = clamp(CartesianIndex(round.(Int,x .+ 1.5f0)...),R)
-Base.clamp(I::CartesianIndex,R::CartesianIndices) = CartesianIndex(clamp.(I.I,first(R).I,last(R).I))
+@fastmath @inline biotsavart(r,j) = Float32((3-2j)*@inbounds(r[j])/(2π*r'*r))
+@fastmath @inline r(x,I,dx) = x-dx*loc(0,I,Float32)
+@fastmath @inline inR(x,R) = clamp(CartesianIndex(round.(Int,x .+ 1.5f0)...),R)
+@inline Base.clamp(I::CartesianIndex,R::CartesianIndices) = CartesianIndex(clamp.(I.I,first(R).I,last(R).I))
 
 function biotBC!(u,U,ml_ω)
     # fill top level with cell-centered ω₃ and restrict down to lower levels
-    @loop ml_ω[1][I] = centered_ω₃(I,u) over I ∈ inside(ml_ω[1],buff=2)
+    top = ml_ω[1]
+    @loop top[I] = centered_ω₃(I,u) over I ∈ inside(top,buff=2)
     ml_restrict!(ml_ω)
 
     # fill BCs
     N,n = size_u(u)
     for i ∈ 1:n
-        for s ∈ (1,2,N[i]) # Normal direction, biotsavart+background
+        for s ∈ (2,N[i]) # Normal direction, biotsavart+background
             @loop u[I,i] = u_ω(i,loc(i,I,Float32),ml_ω)+U[i] over I ∈ slice(N,s,i)
         end
         j = i%2+1          # Tangential direction, ω=0
         @loop u[I,j] = u[I+δ(i,I),j]-WaterLily.∂(j,CartesianIndex(I+δ(i,I),i),u) over I ∈ slice(N.-1,1,i,3)
         @loop u[I,j] = u[I-δ(i,I),j]+WaterLily.∂(j,CartesianIndex(I,i),u) over I ∈ slice(N.-1,N[i],i,3)
+        # final Normal direction, incompresibility
+        @loop u[I,i] = u[I+δ(i,I),i]+WaterLily.∂(j,I,u) over I ∈ slice(N.-1,1,i,3)
     end
 end
 centered_ω₃(I,u) = permute((j,k)->WaterLily.∂(k,j,I,u),3)
@@ -107,20 +110,19 @@ function biot_mom_step!(a,b,ml;use_biotsavart=true)
     WaterLily.BDIM!(a); WaterLily.scale_u!(a,0.5)
     biot_project!(a,b,ml;use_biotsavart,w=0.5)
     push!(a.Δt,WaterLily.CFL(a))
-    maximum(a.u)
 end
 function biot_project!(a::Flow{n},ml_b::MultiLevelPoisson,ml_ω;w=1,use_biotsavart=true,log=false,tol=1e-3,itmx=32) where n
     use_biotsavart ? biotBC!(a.u,a.U,ml_ω) : BC!(a.u,a.U) 
     b = ml_b.levels[1]; @inside b.z[I] = WaterLily.div(I,a.u)
     dt = w*a.Δt[end]; b.x .*= dt
     WaterLily.residual!(b); r₂ = L₂(b); nᵖ = 0;
-    use_biotsavart && (update_resid!(b,b.x,ml_ω))
+    use_biotsavart && (update_resid!(b.r,b.L,b.x,ml_ω))
     while r₂>tol && nᵖ<itmx
         ml_ω[1] .= b.x
         WaterLily.Vcycle!(ml_b)
         WaterLily.smooth!(b)
         b.ϵ .= b.x .- ml_ω[1]; ml_ω[1] .= 0
-        use_biotsavart && update_resid!(b,b.ϵ,ml_ω)
+        use_biotsavart && update_resid!(b.r,b.L,b.ϵ,ml_ω)
         r₂ = L₂(b); nᵖ+=1
         log && @show nᵖ,r₂
     end
@@ -131,40 +133,53 @@ function biot_project!(a::Flow{n},ml_b::MultiLevelPoisson,ml_ω;w=1,use_biotsava
     use_biotsavart ? biotBC!(a.u,a.U,ml_ω) : BC!(a.u,a.U) 
     b.x ./= dt
 end
-function update_resid!(b,ϵ,ml_ω)
+function update_resid!(r,L,ϵ,ml_ω)
     # get pressure-induced vorticity
-    @loop ml_ω[1][I] = ω_from_p(I,b.L,ϵ) over I ∈ inside(ml_ω[1],buff=2)
+    top = ml_ω[1]
+    @loop top[I] = ω_from_p(I,L,ϵ) over I ∈ inside(top,buff=2)
     ml_restrict!(ml_ω)
 
     # update residual on boundaries
-    N,n = size_u(b.L)
+    N,n = size_u(L);
     for i ∈ 1:n
-        @loop b.r[I] -= u_ω(i,loc(i,I,Float32),ml_ω) over I ∈ slice(N.-1,2,i,2)
-        @loop b.r[I] += u_ω(i,loc(i,I+δ(i,I),Float32),ml_ω) over I ∈ slice(N.-1,N[i]-1,i,2)
+        @loop r[I] -= u_ω(i,loc(i,I,Float32),ml_ω) over I ∈ slice(N.-1,2,i,2)
+        @loop r[I] += u_ω(i,loc(i,I+δ(i,I),Float32),ml_ω) over I ∈ slice(N.-1,N[i]-1,i,2)
     end
 
     # correct global resid
-    res = sum(b.r)/sum(2 .* (N .- 2))
+    res = sum(r)/sum(2 .* (N .- 2))
     for i ∈ 1:n
-        @loop b.r[I] -= res over I ∈ slice(N.-1,2,i,2)
-        @loop b.r[I] -= res over I ∈ slice(N.-1,N[i]-1,i,2)
+        @loop r[I] -= res over I ∈ slice(N.-1,2,i,2)
+        @loop r[I] -= res over I ∈ slice(N.-1,N[i]-1,i,2)
     end
 end 
-function ω_from_p(I::CartesianIndex,L,ϵ)
-    u(I,i) = @inbounds(-L[I,i]*WaterLily.∂(i,I,ϵ))
-    ∂(i,j,I,u) = (u(I+δ(j,I),i)+u(I+δ(j,I)+δ(i,I),i)
+@fastmath function ω_from_p(I::CartesianIndex,L,ϵ)
+    @inline u(I,i) = @inbounds(-L[I,i]*WaterLily.∂(i,I,ϵ))
+    @inline ∂(i,j,I,u) = (u(I+δ(j,I),i)+u(I+δ(j,I)+δ(i,I),i)
                  -u(I-δ(j,I),i)-u(I-δ(j,I)+δ(i,I),i))/4
     return permute((j,k)->∂(k,j,I,u),3)
 end
 
 # Check pressure solver convergence on circle
 include("examples/TwoD_plots.jl")
-circ(D,U=1,m=11D÷8) = Simulation((2D,m), (U,0), D; body=AutoBody((x,t)->√sum(abs2,x .- m/2)-D/2),ν=U*D/1e4)
+circ(D,U=1,m=11D÷8;mem=Array) = Simulation((2D,m), (U,0), D; body=AutoBody((x,t)->√sum(abs2,x .- m/2)-D/2),ν=U*D/1e4,mem)
 sim = circ(256); ml = MLArray(sim.flow.σ);
 while sim_time(sim)<1.2
     biot_mom_step!(sim.flow,sim.pois,ml)
     sim_time(sim)%0.1<sim.flow.Δt[end]/sim.L && @show sim_time(sim),sim.flow.Δt[end],sim.pois.n[end]
 end
-flood(sim.flow.p,border=:none,legend=false,clims=(-1.5,1.5))
+flood(sim.flow.p|>Array,border=:none)
 @inside sim.flow.σ[I] = centered_ω₃(I,sim.flow.u)*sim.L/sim.U
-flood(sim.flow.σ,border=:none,legend=false,clims=(-25,25))
+flood(sim.flow.σ|>Array,border=:none,legend=false,clims=(-25,25))
+
+using BenchmarkTools
+using CUDA
+circ(D,U=1,m=2D;mem=Array) = Simulation((2D,m), (U,0), D; body=AutoBody((x,t)->√sum(abs2,x .- m/2)-D/2),ν=U*D/1e4,mem)
+sim = circ(2048;mem=CUDA.CuArray); a = sim.flow; b = sim.pois.levels[1]; ml = MLArray(a.σ);
+@btime CUDA.@sync WaterLily.conv_diff!($a.f,$a.u⁰,$a.σ,ν=$a.ν); # 80ms, 25ms
+@btime CUDA.@sync WaterLily.BDIM!($a); # 25ms, 14ms
+@btime CUDA.@sync biotBC!($a.u,$a.U,$ml) # 60ms, 530ms
+@btime CUDA.@sync update_resid!($b.r,$b.L,$b.x,$ml) # 80ms, 530ms
+@btime CUDA.@sync sum($b.r) # 2.6ms, 1ms
+@btime CUDA.@sync WaterLily.Vcycle!($sim.pois) # 140ms. 34ms
+@btime CUDA.@sync WaterLily.smooth!($b) # 230ms, 54ms
