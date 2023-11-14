53b13e35161451766495b006d4a5ad6d66da9a2f
weymouth <gabriel.weymouth>
2023-11-14 23:46:07 +0100

    Update MWE.jl
    Very very close to the true example now, but still haven't found the issue. Using `loc(0,I)` does slow things down a lot on the GPU, but not 20x!

--- MWE.jl
+++ MWE.jl
@@ -5,31 +5,41 @@ u_cu = CUDA.zeros(N,N,N);
 ω = ntuple(i->(m=N÷2^(i-1);rand(Float32,m,m,m)),5);
 u = Array(u_cu);
 
-function u_ω(J,ω)
+import WaterLily
+function u_ω(j,J,ω)
+    x = WaterLily.loc(j,J,Float32)
     s = 0f0
-    for ωᵢ in ω
-        R = CartesianIndices(ωᵢ)
-        for I ∈ (J-3oneunit(J)):(J+3oneunit(J))
-            I ∈ R && (r = loc(J)-loc(I)-SA_F32[0.5,0,0]; s += ωᵢ[I]*r[1]/√sum(abs2,r)^3)
+    R = CartesianIndices(ω[end])
+    for l ∈ lastindex(ω):-1:2
+        dx = 2f0^(l-1)
+        Rclose = inR(x/dx .- 1,R):inR(x/dx .+ 1,R)
+        for I ∈ R
+            !(I ∈ Rclose) && (rI = r(x,I,dx); s += ω[l][I]*rI[j%3+1]/√sum(abs2,rI)^3)
         end
+        R = (2first(Rclose)-oneunit(J)):2last(Rclose)
     end
-    return s/Float32(4π)
+    for I ∈ R
+        rI = r(x,I); s += ω[1][I]*rI[j%3+1]/√sum(abs2,rI)^3
+    end
+return s/Float32(4π)
 end
-loc(I::CartesianIndex) = SA_F32[I.I...];
+r(x,I::CartesianIndex,dx=1) = x-dx*(SA_F32[I.I...] .- 1.5f0)#WaterLily.loc(0,I,Float32)
+inR(x,R) = clamp(CartesianIndex(round.(Int,x .+ 1.5f0)...),R)
+Base.clamp(I::CartesianIndex,R::CartesianIndices) = CartesianIndex(clamp.(I.I,first(R).I,last(R).I))
 
 R = CartesianIndices((1:N,1:1,1:N));
-u_cu[R] .= u_ω.(R,Ref(ω_cu));
+u_ω(2,CartesianIndex(96,1,64),ω)
+u[R] .= u_ω.(Ref(2),R,Ref(ω));
 
 using KernelAbstractions
-@kernel function kern(u,@Const(ω))
+@kernel function kern(u,@Const(j),@Const(ω))
     J = @index(Global,Cartesian)
-    u[J]=u_ω(J,ω)
+    u[J]=u_ω(j,J,ω)
 end
-u_ω_kern(u,ω,R) = kern(get_backend(u),64)(u,ω,ndrange=size(R))
-u_ω_kern(u_cu,ω_cu,R);
+u_ω_kern(u,j,ω,R) = kern(get_backend(u),64)(u,j,ω,ndrange=size(R))
+u_ω_kern(u_cu,2,ω_cu,R);
 
 using BenchmarkTools
-@benchmark CUDA.@sync $u_cu[$R] .= u_ω.($R,Ref($ω_cu))
-@benchmark $u[$R] .= u_ω.($R,Ref($ω))
-@benchmark CUDA.@sync u_ω_kern($u_cu,$ω_cu,$R)
-@benchmark u_ω_kern($u,$ω,$R)
+@btime $u[$R] .= u_ω.(Ref(2),$R,Ref($ω));
+@btime u_ω_kern($u,2,$ω,$R);
+@btime CUDA.@sync u_ω_kern($u_cu,2,$ω_cu,$R); 
