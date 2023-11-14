2451191cc4e9919278b19afcf99bbfad8321fe6f
marinlauber <marinlauber@tudelft.nl>
2023-11-14 08:46:39 +0100

    updated VIV example to remove global variables

--- examples/TwoD_cylinderVIV.jl
+++ examples/TwoD_cylinderVIV.jl
@@ -3,73 +3,69 @@ using Plots; gr()
 using StaticArrays
 include("TwoD_plots.jl")
 
-# parameters
-Re = 250
-U = 1
-nx, ny = 3*2^6, 2^7
-radius, center = ny/8, ny/2
+# required to keep things global
+let 
+    # parameters
+    Re = 250; U = 1
+    p = 5; L = 2^p
+    radius, center = L/2, 2L
 
-# fsi parameters
-T = 10*radius
-amp = 0.354
-mₐ = π*radius^2  # added-mass coefficent circle http://brennen.caltech.edu/fluidbook/basicfluiddynamics/unsteadyflows/addedmass/valuesoftheaddedmass.pdf
-m = 0.0*mₐ       # zero actual mass
-k = (2*pi/T)^2*(m+mₐ)
+    # fsi parameters
+    T = 4*radius    # VIV period
+    mₐ = π*radius^2 # added-mass coefficent circle
+    m = 0.1*mₐ      # mass as a fraction of the added-mass, can be zero
+    k = (2*pi/T)^2*(m+mₐ)
 
-# initial conditions
-pos = amp*20
-vel = 0; a0 = 0; t_init = 0
+    # initial condition FSI
+    p0=radius/3; v0=0; a0=0; t0=0
 
-# motion function uses global var to adjustset the velocity
-# to the one computed from the FSI, this is because the velocity
-# is computed internaly using ForwardDiff
-y(t) = pos + (t-t_init)*vel
-function map(x,t)
-    x - SA[0,y(t)]
-end
+    # motion function uses global var to adjust
+    posx(t) = p0 + (t-t0)*v0
 
-# make a body
-circle = AutoBody((x,t)->√sum(abs2, x .- center) - radius, map)
+    # motion definition
+    map(x,t) = x - SA[0, posx(t)]
 
-# generate sim
-sim = Simulation((nx,ny), (U,0), radius; ν=U*radius/Re, body=circle)
+    # mₐke a body
+    circle = AutoBody((x,t)->√sum(abs2, x .- center) - radius, map)
 
-# get start time
-duration = 20; step = 0.1
-t₀ = round(sim_time(sim))
-@time @gif for tᵢ in range(t₀,t₀+duration;step)
+    # generate sim
+    sim = Simulation((6L,4L), (U,0), radius; ν=U*radius/Re, body=circle)
 
-    # update until time tᵢ in the background, equivalent to
-    # sim_step!(sim,tᵢ;remeasure=true)
-    t = sum(sim.flow.Δt[1:end-1])
-    while t < tᵢ*sim.L/sim.U
+    # get start time
+    duration=10; step=0.1; t₀=round(sim_time(sim))
 
-        # measure body
-        measure!(sim,t)
+    @time @gif for tᵢ in range(t₀,t₀+duration;step)
 
-        # update flow
-        mom_step!(sim.flow,sim.pois)
+        # update until time tᵢ in the background
+        t = sum(sim.flow.Δt[1:end-1])
+        while t < tᵢ*sim.L/sim.U
+
+            # measure body
+            measure!(sim,t)
+
+            # update flow
+            mom_step!(sim.flow,sim.pois)
+            
+            # pressure force
+            force = -WaterLily.∮nds(sim.flow.p,sim.flow.f,sim.body,t)
+            
+            # compute motion and acceleration 1DOF
+            Δt = sim.flow.Δt[end]
+            accel = (force[2]- k*p0 + mₐ*a0)/(m + mₐ)
+            p0 += Δt*(v0+Δt*accel/2.) 
+            v0 += Δt*accel
+            a0 = accel
+
+            # update time, sets the pos/v0 correctly
+            t0 = t; t += Δt
+        end
+
+        # plot vorticity
+        @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
+        flood(sim.flow.σ; shift=(-0.5,-0.5),clims=(-5,5))
+        body_plot!(sim); plot!(title="tU/L $tᵢ")
         
-        # pressure force
-        force = -WaterLily.∮nds(sim.flow.p,sim.flow.f,circle,t)
-        
-        # compute motion and acceleration 1DOF
-        Δt = sim.flow.Δt[end]
-        accel = (force[2]- k*pos + mₐ*a0)/(m + mₐ)
-        global pos += Δt*(vel+Δt*accel/2.) 
-        global vel += Δt*accel
-        global a0 = accel
-        
-        # update time, must be done globaly to set the pos/vel correctly
-        global t_init = t
-        t += Δt
+        # print time step
+        println("tU/L=",round(tᵢ,digits=4),", Δt=",round(sim.flow.Δt[end],digits=3))
     end
-
-    # plot vorticity
-    @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
-    flood(sim.flow.σ; shift=(-0.5,-0.5),clims=(-5,5))
-    body_plot!(sim)
-    
-    # print time step
-    println("tU/L=",round(tᵢ,digits=4),", Δt=",round(sim.flow.Δt[end],digits=3))
 end
