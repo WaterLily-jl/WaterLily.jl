2b6f38e2db2d70491b14ba83425fd0e34c459e33
weymouth <gabriel.weymouth>
2023-11-15 00:02:52 +0100

    Update MWE.jl
    Closers still. VERY important to get the biotsavart inlined. Maybe that's it...?

--- MWE.jl
+++ MWE.jl
@@ -10,16 +10,17 @@ function u_ω(j,J,ω)
     x = WaterLily.loc(j,J,Float32)
     s = 0f0
     R = CartesianIndices(ω[end])
+    @inline biotsavart(r,I,l) = ω[l][I]*r[j%3+1]/√sum(abs2,r)^3
     for l ∈ lastindex(ω):-1:2
         dx = 2f0^(l-1)
         Rclose = inR(x/dx .- 1,R):inR(x/dx .+ 1,R)
         for I ∈ R
-            !(I ∈ Rclose) && (rI = r(x,I,dx); s += ω[l][I]*rI[j%3+1]/√sum(abs2,rI)^3)
+            !(I ∈ Rclose) && (s += biotsavart(r(x,I,dx),I,l))
         end
         R = (2first(Rclose)-oneunit(J)):2last(Rclose)
     end
     for I ∈ R
-        rI = r(x,I); s += ω[1][I]*rI[j%3+1]/√sum(abs2,rI)^3
+        s += biotsavart(r(x,I),I,1)
     end
 return s/Float32(4π)
 end
@@ -30,16 +31,9 @@ Base.clamp(I::CartesianIndex,R::CartesianIndices) = CartesianIndex(clamp.(I.I,fi
 R = CartesianIndices((1:N,1:1,1:N));
 u_ω(2,CartesianIndex(96,1,64),ω)
 u[R] .= u_ω.(Ref(2),R,Ref(ω));
-
-using KernelAbstractions
-@kernel function kern(u,@Const(j),@Const(ω))
-    J = @index(Global,Cartesian)
-    u[J]=u_ω(j,J,ω)
-end
-u_ω_kern(u,j,ω,R) = kern(get_backend(u),64)(u,j,ω,ndrange=size(R))
-u_ω_kern(u_cu,2,ω_cu,R);
+u_ω_loop(u,j,ω,R) = WaterLily.@loop u[I]=u_ω(j,I,ω) over I ∈ R
 
 using BenchmarkTools
 @btime $u[$R] .= u_ω.(Ref(2),$R,Ref($ω));
-@btime u_ω_kern($u,2,$ω,$R);
-@btime CUDA.@sync u_ω_kern($u_cu,2,$ω_cu,$R); 
+@btime u_ω_loop($u,2,$ω,$R);
+@btime CUDA.@sync u_ω_loop($u_cu,2,$ω_cu,$R); 
