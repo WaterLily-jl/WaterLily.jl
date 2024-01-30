a9ff87c88b7ff80ac4ae2c8e2fd5806a6dacd785
marinlauber <marinlauber@gmail.com>
2024-01-30 17:11:07 +0100

    updated examples

--- examples/TwoD_SlowStartCircle.jl
+++ examples/TwoD_SlowStartCircle.jl
@@ -1,12 +1,12 @@
 using WaterLily
-function circle(m,n;a0=0.5,Re=250,U=1)
+using CUDA
+function circle(m,n;a0=0.5,Re=250,U=1,f=Array)
     # define a circle at the domain center
     radius = n/8
     body = AutoBody((x,t)->√sum(abs2, x .- (m/4,n/2)) - radius)
-
-    # define time-varying body force `g`
-    g(i,t) = i==1 ? a0.*(1.0.-tanh.(31.4.*(t.-1.0./a0)))/2. : 0
-    Simulation((m,n), (0,0), radius; U, ν=U*radius/Re, body, g)
+    # define time-varying velocity boundary conditions
+    Ut(i,t) = i==1 ? a0.*t+(1.0.+tanh.(31.4.*(t.-1.0./a0)))/2.0*(1-a0*t) : 0
+    Simulation((m,n), (0,0), radius; U, Uₜ=Ut, ν=U*radius/Re, body, mem=f)
 end
 include("TwoD_plots.jl")
 sim = circle(2*196,196)
