437b4823350ee7f92583e4eb329c994e52a3957f
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-04-10 17:29:27 +0300

    copy all .jpg/.gif files before building docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,25 @@
 using Documenter, WaterLily
 
+## find all image files in examples/ dir and copy in docs/src/examples/
+
+recursive_find(directory, pattern) =
+    mapreduce(vcat, walkdir(directory)) do (root, dirs, files)
+        joinpath.(root, filter(contains(pattern), files))
+    end
+
+image_files = []
+
+for pattern in [r"\.gif", r"\.jpg"]
+    global image_files = vcat(image_files, recursive_find(joinpath(@__DIR__, "../examples"), pattern))
+end
+
+for file in image_files
+    cp(file, joinpath(@__DIR__, "src/examples/"*basename.(file)), force=true)
+end
+
+
+## now ready to make the docs
+
 makedocs(
     modules = [WaterLily],
     format=Documenter.HTML(;
