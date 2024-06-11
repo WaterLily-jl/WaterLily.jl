c76dd0388561e93b719dde69161852d111846231
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2024-06-11 17:25:50 +0200

    Update test with non-mutating step_force function

--- test.jl
+++ test.jl
@@ -1,30 +1,40 @@
 using WaterLily,StaticArrays
 
-global θ₀=π/36
-global h₀=0
-
-function make_sim(;L=32,U=1,Re=100,ϵ=0.5,thk=2ϵ+√2,T=Float64)
-    cen = SA[L,L]
-    h(t) = h₀
-    θ(t) = θ₀
-    function map(x,t)
-        s,c = sincos(θ(t))
-        SA[c -s; s c]*(x-cen-SA[0,h(t)])
-    end
-    function sdf(ξ,t) # Line segment
-        p = ξ-SA[0,clamp(ξ[1],-L/2,L/2)] 
-        √(p'*p)-thk/2
-    end
-    Simulation((2L,2L),(U,0),L;ν=U*L/Re,body=AutoBody(sdf,map),ϵ,T)
+function map(x,t;L,θ)
+    s,c = sincos(θ)
+    SA[c -s; s c]*(x-SA[L,L])
 end
-function step_force!(sim,θ)
-    global θ₀ = θ
+function sdf(ξ,t;L)
+    p = ξ-SA[0,clamp(ξ[1],-L/2,L/2)] 
+    √(p'*p)-2
+end
+function make_sim(θ;L=32,U=1,Re=100,thk=2+√2,T=Float64)
+    body=AutoBody((a,b)->sdf(a,b;L,thk),(a,b)->map(a,b;L,θ))
+    Simulation((2L,2L),(U,0),L;ν=U*L/Re,body,ϵ,T)
+end
+function step_force!(sim)
     sim_step!(sim)
     sum(sim.flow.p)
 end
 using ForwardDiff: derivative, Dual, Tag
-R = Float64
-T = typeof(Tag(make_sim, R))
-sim = make_sim(;T);
-# lift_hist = [step_force!(sim,π/36) for _ ∈ 1:20]
-step_force!(sim,Dual{Float64}(π/36,1))
+T = Float64 #typeof(Tag(make_sim, Float64))
+sim = make_sim(π/36;T);
+lift_hist = [step_force!(sim) for _ ∈ 1:20]
+
+function step_force(θ,sim₀)
+    @show θ
+    t = WaterLily.timeNext(sim₀.flow)
+    R = inside(sim₀.flow.p)
+    body = AutoBody((a,b)->sdf(a,b;sim₀.L),(a,b)->map(a,b;sim₀.L,θ))
+    dᵢ,nᵢ,Vᵢ = measure(body,WaterLily.loc(1,first(R)),t)
+    @show dᵢ
+    @show nᵢ
+    @show Vᵢ  
+    # flow = Flow()
+    # measure!(a::Flow{N},body::AbstractBody;t)
+    # mom_step!(flow,MultiLevelPoisson(flow.p,flow.μ₀,flow.σ))
+    # sum(flow.p)
+    dᵢ
+end
+derivative(θ->step_force(θ,sim),π/36)
+step_force(Dual{typeof(Tag(step_force, Float64))}(π/36,1),sim)
