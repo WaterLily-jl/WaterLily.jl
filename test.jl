79cf79068029f045aa17134c6da84d1d6e9254d8
weymouth <gabriel.weymouth@gmail.com>
2024-06-12 20:56:10 +0200

    Working test,jl
    I used a Ref to wrap theta such that it updates the Simulation automatically without needing to deconstruct it or anything. This actually vastly cleans up both the Finite Difference and AD examples. The workflow is really clean now and the results match.

--- test.jl
+++ test.jl
@@ -1,38 +1,33 @@
 using WaterLily,StaticArrays
 
-function map(x,t;L,θ)
-    s,c = sincos(θ)
-    SA[c -s; s c]*(x-SA[L,L])
-end
-function sdf(ξ,t;L)
-    p = ξ-SA[0,clamp(ξ[1],-L/2,L/2)]
-    √(p'*p)-2
-end
 function make_sim(θ;L=32,U=1,Re=100)
-    T=typeof(θ)
-    body=AutoBody((a,b)->sdf(a,b;L),(a,b)->map(a,b;L,θ))
-    Simulation((2L,2L),(U,0),L;ν=U*L/Re,body,T)
+    function map(x,t)
+        s,c = sincos(θ[])
+        SA[c -s; s c]*(x-SA[L,L])
+    end    
+    function sdf(ξ,t)
+        p = ξ-SA[0,clamp(ξ[1],-L/2,L/2)]
+        √(p'*p)-2
+    end
+    Simulation((2L,2L),(U,0),L,ν=U*L/Re,body=AutoBody(sdf,map),T=typeof(θ[]))
 end
 
-function step_force!(θ,sim,t=WaterLily.timeNext(sim.flow))
-    body = AutoBody((a,b)->sdf(a,b;sim.L),(a,b)->map(a,b;sim.L,θ))
-    measure!(sim.flow,body;t,ϵ=sim.ϵ)
-    WaterLily.update!(sim.pois)
-    mom_step!(sim.flow,sim.pois)
-    sum(sim.flow.p)
-end
-sim = make_sim(π/36);
-lift_hist = [step_force!(π/36,sim) for _ ∈ 1:20]
-a = step_force!(π/36+1e-4,deepcopy(sim));
-b = step_force!(π/36-1e-4,deepcopy(sim));
-println("FD grad = ", (a-b)/2e-4)
-
-function step_force(θ,sim⁰)
-    sim = make_sim(θ)
-    sim.flow.u .= sim⁰.flow.u; sim.flow.p .= sim⁰.flow.p;
-    sim.flow.Δt .= WaterLily.time(sim⁰.flow); push!(sim.flow.Δt,sim⁰.flow.Δt[end])
+function step_force!(sim)
     sim_step!(sim)
     sum(sim.flow.p)
 end
-using ForwardDiff: derivative
-println("AD grad = ", derivative(x->step_force(x,deepcopy(sim)),π/36))
+θ = Ref(π/36) # wrap the parameter in a Ref so it can be updated
+sim = make_sim(θ);
+lift_hist = [step_force!(sim) for _ ∈ 1:20]
+θ[] = π/36+1e-4; a = step_force!(deepcopy(sim)); # use a copy to avoid updating sim
+θ[] = π/36-1e-4; b = step_force!(deepcopy(sim)); # use a copy to avoid updating sim
+θ[] = π/36; c = step_force!(sim);
+println("sim value and FD partial = ", (c,(a-b)/2e-4))
+
+using ForwardDiff: Dual,Tag
+T = typeof(Tag(step_force!, Float64)) # make a tag
+θAD = Ref(Dual{T}(π/36, 0.))          # wrap the Dual parameter in a Ref
+simAD = make_sim(θAD);                # make a sim of the correct type
+lift_histAD = [step_force!(simAD) for _ ∈ 1:20] # still works
+θAD[] = Dual{T}(π/36,1.)              # update partial to take derivative
+println("simAD = ", step_force!(simAD))
