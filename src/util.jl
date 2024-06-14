573a74a224e7e91284e65c851732b96a2fec1834
Bernat Font <bernatfontgarcia@gmail.com>
2024-06-14 18:58:02 +0200

    Replaced Rational numbers in loc for Float64 numbers. Note that some tests were not passing when using Float32 in loc since they were comparing Float64 to Float32. This can be changed to Float32 then, but tests should be modified accordingly.

--- src/util.jl
+++ src/util.jl
@@ -133,7 +133,7 @@ using StaticArrays
 Location in space of the cell at CartesianIndex `I` at face `i`.
 Using `i=0` returns the cell center s.t. `loc = I`.
 """
-@inline loc(i,I::CartesianIndex{N}) where N = SVector{N}(I.I .- 3//2 .- 1//2 .* δ(i,I).I)
+@inline loc(i,I::CartesianIndex{N}) where N = SVector{N}(I.I .- 1.5 .- 0.5 .* δ(i,I).I)
 @inline loc(Ii::CartesianIndex) = loc(last(Ii),Base.front(Ii))
 Base.last(I::CartesianIndex) = last(I.I)
 Base.front(I::CartesianIndex) = CI(Base.front(I.I))
