7fdbdd82cd50b4118d37f5471198477e624b74b2
marinlauber <marinlauber@gmail.com>
2024-07-12 11:22:42 +0200

    MPIArray working with sims

--- MPIArray.jl
+++ MPIArray.jl
@@ -17,33 +17,32 @@ for fname in (:size, :length, :ndims, :eltype) # how to write 4 lines of code in
         Base.$fname(A::MPIArray) = Base.$fname(A.A)
     end
 end
-Base.getindex(A::MPIArray, i::Int...) = Base.getindex(A.A, i...)
-Base.getindex(A::MPIArray, I::CartesianIndices...) = Base.getindex(A.A, I...)
-Base.setindex!(A::MPIArray, v, i...)  = Base.setindex!(A.A, v, i...)
+Base.getindex(A::MPIArray, i...)            = Base.getindex(A.A, i...)
+Base.setindex!(A::MPIArray, v, i...)        = Base.setindex!(A.A, v, i...)
 Base.copy(A::MPIArray) = (B=MPIArray(eltype(A),size(A)); B.A.=A.A; B)
-Base.similar(A::MPIArray) = MPIArray(eltype(A),size(A))
-Base.similar(A::MPIArray, dims::Tuple) = MPIArray(eltype(A),dims)
-KernelAbstractions.get_backend(::MPIArray) = CPU()
-
-# function write_h2h!(A::MPIArray, N=size(A))
-#     @inbounds copyto!(view(A.send[:]), view(A.A[halos(N,j)]))
-# end
-
-# function read_h2h!(A::MPIArray, N=size(A))
-#     @inbounds copyto!(view(A.A[buff()]), view(A.send[1,:]))
-# end
+Base.similar(A::MPIArray)                   = MPIArray(eltype(A),size(A))
+Base.similar(A::MPIArray, dims::Tuple)      = MPIArray(eltype(A),dims)
+# required for the @loop function
+KernelAbstractions.get_backend(A::MPIArray) = KernelAbstractions.get_backend(A.A)
 
 # # initialize array
 Nd = (10,10,2)
 u = Array{Float64}(undef, Nd...) |> MPIArray
 p = zeros(Base.front(Nd)...) |> MPIArray
 
+# circle simulation
 function circle(n,m;Re=250,U=1)
     radius, center = m/8, m/2
     body = AutoBody((x,t)->√sum(abs2, x .- center) - radius)
     Simulation((n,m), (U,0), radius; ν=U*radius/Re, body, mem=MPIArray)
 end
 
+# test on sim
 include("examples/TwoD_plots.jl")
 sim = circle(3*2^6,2^7)
 sim_gif!(sim,duration=10,clims=(-5,5),plotbody=true)
+
+# @btime mom_step!($sim.flow,$sim.pois)      # with MPIArray
+#   5.015 ms (24899 allocations: 1.87 MiB)
+# @btime mom_step!($sim.flow,$sim.pois)      # with Array
+#   2.649 ms (22285 allocations: 1.55 MiB)
