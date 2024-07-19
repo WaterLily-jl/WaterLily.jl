3adc9d057dc8f67c074a8570b11bdc4cae0f3cd2
Bernat Font <bernatfontgarcia@gmail.com>
2024-07-19 14:21:59 +0200

    Updated gifs in README (and examples related to the gifs) and cleaned up text in README

--- examples/TwoD_Julia.jl
+++ examples/TwoD_Julia.jl
@@ -1,13 +1,17 @@
 using WaterLily
+using StaticArrays
+using CUDA
+
 include("TwoD_plots.jl")
-function TwoD_julia_video(;p=6,Re=250,stop=60.)
+
+function run_TwoD_julia(;p=6,Re=400,stop=100.)
     # Set simulation size & physical parameters
     n,m = 2^p,2^p
     U,R,r = 1, m/16, m/16/0.75
 
     # Immerse three well placed circles (change for other shapes)
-    z = [R*exp(im*θ) for θ ∈ range(0,2π,length=33)]
-    centers = [n/2+im*3*m/4+r*exp(im*ϕ) for ϕ ∈ [π/2,π/2+2π/3,π/2-2π/3]]
+    z = @SArray [R*exp(im*θ) for θ ∈ range(0,2π,length=33)]
+    centers = @SArray [2n/2+im*14*m/4+r*exp(im*ϕ) for ϕ ∈ [π/2,π/2+2π/3,π/2-2π/3]]
     colors = [:forestgreen,:brown3,:mediumorchid3]
     body = AutoBody() do x,t  # signed distance function
             minimum(centers) do center
@@ -15,19 +19,23 @@ function TwoD_julia_video(;p=6,Re=250,stop=60.)
             end
     end
 
-    # Initialize simulation
-    sim = Simulation((n,m),(0,-U),R;ν=U*R/Re,body)
+    # Initialize simulation on the GPU and a CPU buffer array for plotting
+    sim = Simulation((2n,4m),(0,-U),R;ν=U*R/Re,body,mem=CuArray)
+    σ = zeros(size(sim.flow.σ))
 
     # Solve flow and make nice gif
-    
-    @time @gif for tᵢ in range(0.,stop;step=0.3)
+    @time @gif for tᵢ in range(0.,stop;step=0.5)
         println("tU/L=",round(tᵢ,digits=4))
         sim_step!(sim,tᵢ)
         @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*R/U
-        flood(sim.flow.σ,shift=(-0.5,-0.5),clims=(-5,5),
-            cfill=:Blues,legend=false,border=:none)
+        @inside sim.flow.σ[I] = ifelse(abs(sim.flow.σ[I])<0.001,0.0,sim.flow.σ[I])
+        copyto!(σ,sim.flow.σ)
+        flood(σ,shift=(-2,-1.5),clims=(-5,5),
+            cfill=:seismic,legend=false,border=:none,size=(2n,4m))
         for (center,color) ∈ zip(centers,colors)
             addbody(real(z.+center),imag(z.+center),c=color)
         end
     end
 end
+
+run_TwoD_julia(;p=8)
--- examples/TwoD_hover.jl
+++ examples/TwoD_hover.jl
@@ -1,6 +1,9 @@
 using WaterLily
 using StaticArrays
-function hover(L=2^5;Re=250,U=1,amp=π/4,ϵ=0.5,thk=2ϵ+√2)
+
+include("TwoD_plots.jl")
+
+function hover(L=2^6;Re=250,U=1,amp=π/4,ϵ=0.5,thk=2ϵ+√2)
     # Line segment SDF
     function sdf(x,t)
         y = x .- SA[0,clamp(x[2],-L/2,L/2)]
@@ -14,10 +17,19 @@ function hover(L=2^5;Re=250,U=1,amp=π/4,ϵ=0.5,thk=2ϵ+√2)
     Simulation((6L,6L),(0,0),L;U,ν=U*L/Re,body=AutoBody(sdf,map),ϵ)
 end
 
-sim = hover();
-sim_step!(sim,π)
-include("TwoD_plots.jl")
-a = sim.flow.σ;
-@inside a[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
-flood(a[inside(a)],clims=(-5,5))
-body_plot!(sim)
+function run_hover(stop=20.)
+    sim = hover()
+    sim_step!(sim,π)
+
+    @time @gif for tᵢ in range(0.,stop;step=0.1)
+        println("tU/L=",round(tᵢ,digits=4))
+        sim_step!(sim,tᵢ)
+        @inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
+        @inside sim.flow.σ[I] = ifelse(abs(sim.flow.σ[I])<0.001,0.0,sim.flow.σ[I])
+        flood(sim.flow.σ,shift=(-2,-1.5),clims=(-5,5), axis=([], false),
+            cfill=:seismic,legend=false,border=:none,size=(6*sim.L,6*sim.L))
+        body_plot!(sim)
+    end
+end
+
+run_hover()
