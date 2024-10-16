1d0a47f3949a3cda7c90cc7d6948530726fde75e
marinlauber <marinlauber@tudelft.nl>
2024-10-16 11:28:50 +0200

    better distance and fix .inp files

--- src/MeshBodies.jl
+++ src/MeshBodies.jl
@@ -9,14 +9,50 @@ struct MeshBody{T} <: AbstractBody
     scale :: T
     bbox  :: Rect
     function MeshBody(fname;map=(x,t)->x,scale=1.0,T=Float32)
-        tmp = load(fname) 
+        tmp = endswith(fname,".inp") ? load_inp(fname) : load(fname)
         points = GeometryBasics.Point.(tmp.position*scale) # can we specify types?
         mesh = GeometryBasics.Mesh(points,GeometryBasics.faces(tmp))
         bbox = Rect(mesh.position)
         bbox = Rect(bbox.origin.-4,bbox.widths.+8)
-        new{T}(mesh,map,scale,bbox)
+        new{typeof(scale)}(mesh,map,scale,bbox)
     end
 end
+
+function load_inp(fname; facetype=GLTriangleFace, pointtype=Point3f)
+    #INP file format
+    @assert endswith(fname,".inp") "file type not supported"
+    fs = open("/home/marin/Workspace/WaterLily/cube.inp")
+
+    points = pointtype[]
+    faces = facetype[]
+    node_idx = Int[]
+
+    # read the first 3 lines if there
+    line = split(lowercase(readline(fs)))
+    (!isempty(line) && line[1] == "*heading") 
+    BlockType = Val{:DataBlock}()
+    line = readline(fs)
+
+    # read the file
+    while !eof(fs)
+        line = readline(fs)
+        BlockType, line = parse_blocktype!(BlockType, fs, line)
+        if BlockType == Val{:NodeBlock}()
+            push!(node_idx, parse(Int,split(line,",")[1])) # keep track of the node index of the inp file
+            push!(points, pointtype(parse.(eltype(pointtype),split(line,",")[2:4])))
+        elseif BlockType == Val{:ElementBlock}()
+            nodes = parse.(Int,split(line,",")[2:end])
+            push!(faces, TriangleFace{Int}(facetype([findfirst(==(node),node_idx) for node in nodes])...)) # parse the face
+        end
+    end
+    return Mesh(points, faces); close(fs);
+end
+function parse_blocktype!(block, io, line)
+    contains(line,"*NODE") && return block=Val{:NodeBlock}(),readline(io)
+    contains(line,"*ELEMENT") && return block=Val{:ElementBlock}(),readline(io)
+    return block, line
+end
+
 """
     locate(p,tri)
 
@@ -75,24 +111,25 @@ function locate(tri::GeometryBasics.Ngon{3},p::SVector{T}) where T #5.327 ns (0 
 end
 
 # inside bbox or not
-inside(bbox::Rect,x::SVector) = all(bbox.origin .≤ x) && all(x .≤ bbox.origin+bbox.widths)
+outside(x::SVector,bbox::Rect) = !(all(bbox.origin .≤ x) && all(x .≤ bbox.origin+bbox.widths))
 rect = Rect(0,0,0,1,1,1) # origin and widths
-# @assert !inside(rect,SA[0.5,1,2.5]) && inside(rect,SA[0.5,0.5,0.5])
+# @assert !inside(SA[0.5,1,2.5],rect) && inside(SA[0.5,0.5,0.5],rect)
 # 
 # distance to box center
-dist(bbox::Rect, x::SVector) = √sum(abs2,x.-bbox.origin-0.5bbox.widths)
+dist(x::SVector,bbox::Rect) = √sum(abs2,x.-bbox.origin-0.5bbox.widths)
 rect = Rect(0,0,0,1,1,1) # origin and widths
-# @assert dist(rect,SA[1.0,1.0,1.0]) == √0.75
-# @assert dist(rect,SA[1.5,1.0,1.0]) == √1.5
-# @assert dist(rect,SA[1.5,1.5,1.0]) == √2.25
-# @assert dist(rect,SA[1.5,1.5,1.5]) == √3.0
+# @assert dist(SA[1.0,1.0,1.0],rect) == √0.75
+# @assert dist(SA[1.5,1.0,1.0],rect) == √1.5
+# @assert dist(SA[1.5,1.5,1.0],rect) == √2.25
+# @assert dist(SA[1.5,1.5,1.5],rect) == √3.0
 
 sdf(body::MeshBody,x,t;kwargs...) = measure(body.mesh,body.map(x,t),t;kwargs...)[1]
 
 function measure(body::MeshBody,x,t;kwargs...)
     # eval d=map(x,t)-x, and n̂
-    ξ = body.map(x,t) # if we are outside of the bouding box, we can measure approx
-    # !inside(body.bbox,ξ) && return (dist(body.bbox,ξ),zero(x),zero(x))
+    ξ = body.map(x,t);
+     # if we are outside of the bouding box, we can measure approx
+    outside(ξ,body.bbox) && return (dist(ξ,body.bbox),zero(x),zero(x))
     d,n = measure(body.mesh,ξ,t)
 
     # The velocity depends on the material change of ξ=m(x,t):
@@ -121,6 +158,6 @@ Measure the distance `d` and normal `n` to the mesh at point `x` and time `t`.
 function measure(mesh::GeometryBasics.Mesh,x::SVector{T},t;kwargs...) where T
     tmp = [d²(mesh[I],x) for I in CartesianIndices(mesh)] # can we awoid this?
     idx = argmin(tmp); n = WaterLily.normal(mesh[idx])
-    d = sum((x-locate(mesh[idx],x)).*n)
+    v = x-locate(mesh[idx],x); d = sign(sum(v.*n))*√sum(abs2,v) # signed Euclidian distance
     d,n
 end
