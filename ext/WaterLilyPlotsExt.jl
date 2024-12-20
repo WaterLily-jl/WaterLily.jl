e9589afa7b62d0c9d0935aac89e162ac243c4fa1
Tzu-Yao Huang (Arch) <tzuyao.jason.huang@gmail.com>
2024-12-20 12:23:41 +0100

    Fix a small bug

--- ext/WaterLilyPlotsExt.jl
+++ ext/WaterLilyPlotsExt.jl
@@ -44,7 +44,7 @@ function sim_gif!(sim;duration=1,step=0.1,verbose=true,R=inside(sim.flow.p),
     @time @gif for tᵢ in range(t₀,t₀+duration;step)
         WaterLily.sim_step!(sim,tᵢ;remeasure)
         @WaterLily.inside sim.flow.σ[I] = WaterLily.curl(3,I,sim.flow.u)*sim.L/sim.U
-        flood(sim.flow.σ[R]; kv...)
+        flood(sim.flow.σ[R]|>Array; kv...)
         plotbody && body_plot!(sim)
         verbose && println("tU/L=",round(tᵢ,digits=4),
                            ", Δt=",round(sim.flow.Δt[end],digits=3))
