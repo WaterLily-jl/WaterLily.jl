4b4745e5c8865793f81ad7b1e7d4d7746b7a33a0
Bernat Font <bernatfontgarcia@gmail.com>
2025-05-02 16:28:22 +0200

    GLMakie extension implementing viz, a general rendering routine for 2D and 3D simulations.

---
+++ ext/WaterLilyGLMakieExt.jl
@@ -0,0 +1,140 @@
+module WaterLilyGLMakieExt
+
+if isdefined(Base, :get_extension)
+    using GLMakie; gr()
+else
+    using ..GLMakie; gr()
+end
+
+using WaterLily
+import WaterLily: viz!
+
+"""
+    update_body!(a_cpu::Array, sim)
+Measure the body SDF and update the CPU buffer arrray.
+"""
+function update_body!(a_cpu::Array, sim)
+    WaterLily.measure_sdf!(sim.flow.σ, sim.body, WaterLily.time(sim))
+    copyto!(a_cpu, sim.flow.σ[inside(sim.flow.σ)])
+end
+"""
+    plot_body_obs!(ax, b::Observable{Array{T,2}} where T; color=:black)
+Plot the 2D body SDF `b::Observable` at distance 0 in a 2D contourf axis.
+"""
+plot_body_obs!(ax, b::Observable{Array{T,2}} where T; color=:black) = Makie.contourf!(ax, b;
+    levels=[0], colormap=[color], extendlow=:auto
+)
+"""
+    plot_body_obs!(ax, b::Observable{Array{T,3}} where T; color=:black, isorange=0.3)
+Plot the 3D body SDF `b::Observable` at distance 0 in a 3D volume axis.
+"""
+plot_body_obs!(ax, b::Observable{Array{T,3}} where T; color=:black, isorange=0.3) = Makie.volume!(ax, b;
+    algorithm=:iso, colormap=[color], isovalue=0, isorange, lowclip=color
+)
+"""
+    plot_σ_obs!(ax, σ::Observable{Array{T,2}} where T; kwargs...)
+Plot the 2D scalar `σ::Observable` in a 2D contour axis.
+"""
+plot_σ_obs!(ax, σ::Observable{Array{T,2}} where T; kwargs...) = Makie.contourf!(ax, σ; overdraw=true, kwargs...)
+"""
+    plot_σ_obs!(ax, σ::Observable{Array{T,3}} where T; kwargs...)
+Plot the 3D scalar `σ::Observable` in a 3D volume axis.
+"""
+plot_σ_obs!(ax, σ::Observable{Array{T,3}} where T; kwargs...) = Makie.volume!(ax, σ; overdraw=true, kwargs...)
+
+"""
+    viz!(sim, f!::Function; t_end=nothing, max_steps=typemax(Int), remeasure=false, verbose=true,
+        d=2, CIs=nothing, cut=nothing, body=true, body_color=:black,
+        video=false, skipframes=1, fname="flow.mp4", hideaxis=false, framerate=30, compression=5, kwargs...
+    )
+
+General visualization routine to simulate and render the flow field using GLMakie.
+Works for both 2D and 3D simulations. For 3D simulations, the user can choose to render 3D volumetric scalar data,
+    or a 2D slice.
+Users must pass a function `f!` used to post-process the flow field data and copy the scalar field into a CPU buffer array.
+The interface of `f!` must follow `f!(cpu_array::Array, sim::AbstractSimulation)`. For example, to visualize vorticity magnitude:
+```
+function f!(cpu_array, sim)
+    a,dt = sim.flow.σ,sim.L/sim.U
+    WaterLily.@inside a[I] = WaterLily.ω_mag(I,sim.flow.u)
+    copyto!(cpu_array, a[inside(a)]) # copy to CPU
+end
+```
+Keyword arguments:
+    - `t_end::Number`: Simulation end time.
+    - `max_steps::Int`: Simulation end time.
+    - `remeasure::Bool`: Update the body position.
+    - `verbose::Bool`: Print simulation information.
+    - `d::Int`: Plot dimension. `d=2` produces a `Makie.contourf`, and `d=3` produces a `Makie.volume`
+    - `CIs::CartesianIndices`: Range of Cartesian indices to render.
+    - `cut::Tuple{Int, Int, Int}`: For 3D simulation and `d=2`, `cut` provides the plane to render. Defaults to (0,0,N[3]/2).
+        It needs to be defined as a Tuple of 0s with a single non-zero entry on the cutting plane.
+    - `body::Bool`: Plot the body.
+    - `body_color`: Color of the body
+    - `video_fname::String`: Save the simulation as as video, instead of rendering. Defaults to `nothing` (not saving video).
+    - `skipframes::Int`: Only render every `skipframes` time steps.
+    - `hideaxis::Bool`: Figures without axis details.
+    - `framerate::Int`: Video framerate.
+    - `compression::Int`: Video compression.
+    - `kwargs`: Additional keyword arguments passed to `plot_σ_obs!`.
+"""
+function viz!(sim, f!::Function; t_end=nothing, max_steps=typemax(Int), remeasure=false, verbose=true,
+    d=2, CIs=nothing, cut=nothing, body=true, body_color=:black,
+    video_fname=nothing, skipframes=1, hideaxis=false, framerate=30, compression=5, kwargs...)
+    function update_data()
+        f!(dat, sim)
+        σ[] = WaterLily.squeeze(dat[CIs])
+        body && remeasure && (update_body!(dat, sim); σb_obs[] = WaterLily.squeeze(dat[CIs]))
+    end
+    D = ndims(sim.flow.σ)
+    @assert d <= D "Cannot do a 3D plot on a 2D simulation."
+
+    isnothing(CIs) && (CIs = CartesianIndices(Tuple(1:n for n in size(inside(sim.flow.σ)))))
+    dat = sim.flow.σ[inside(sim.flow.σ)] |> Array
+    if d != D && all(>(1), length.(CIs.indices)) # Requesting 2D plot on 3D data, and CIs is not a slice
+        isnothing(cut) && (cut = (0, 0, size(dat,3)÷2))
+        @assert count(==(0), cut) == 2 "Requesting 2D plot on 3D data, but `cut` is not an slice, eg: (0,0,10)"
+        cut_dim = findfirst(!=(0), cut)
+        CIs = Tuple(i == cut_dim ? (cut[i]:cut[i]) : CIs.indices[i] for i in 1:D) |> CartesianIndices
+    end
+    limits = Tuple((1,n) for n in size(CIs) if n > 1)
+
+    f!(dat, sim)
+    σ = WaterLily.squeeze(dat[CIs]) |> Observable
+    body && (update_body!(dat, sim); σb_obs = WaterLily.squeeze(dat[CIs]) |> Observable)
+
+    fig = Figure(size=(1200,1200), figure_padding=5)
+    ax = d==2 ? Axis(fig[1, 1]; aspect=DataAspect(), limits) : Axis3(fig[1, 1]; aspect=:data, limits)
+    plot_σ_obs!(ax, σ; kwargs...)
+    body && plot_body_obs!(ax, σb_obs; color=body_color)
+    hideaxis && (hidedecorations!(ax); ax.xspinesvisible = false; ax.yspinesvisible = false; ax.zspinesvisible = false)
+
+    if !isnothing(t_end) # time loop for animation
+        steps₀ = length(sim.flow.Δt)
+        if !isnothing(video_fname)
+            GLMakie.record(fig, video_fname; framerate, compression) do frame
+                while sim_time(sim) < t_end && length(sim.flow.Δt) - steps₀ < max_steps
+                    sim_step!(sim; remeasure)
+                    verbose && WaterLily.sim_info(sim)
+                    if mod(length(sim.flow.Δt), skipframes) == 0
+                        update_data()
+                        recordframe!(frame)
+                    end
+                end
+            end
+        else
+            display(fig)
+            while sim_time(sim) < t_end && length(sim.flow.Δt) - steps₀ < max_steps
+                sim_step!(sim; remeasure)
+                verbose && print_info(sim)
+                if mod(length(sim.flow.Δt), skipframes) == 0
+                    update_data()
+                end
+            end
+            display(fig)
+        end
+    end
+    return sim, fig, ax
+end
+
+end # module
--- src/WaterLily.jl
+++ src/WaterLily.jl
@@ -100,8 +100,7 @@ function sim_step!(sim::AbstractSimulation,t_end;remeasure=true,max_steps=typema
     steps₀ = length(sim.flow.Δt)
     while sim_time(sim) < t_end && length(sim.flow.Δt) - steps₀ < max_steps
         sim_step!(sim; remeasure, udf, kwargs...)
-        verbose && println("tU/L=",round(sim_time(sim),digits=4),
-            ", Δt=",round(sim.flow.Δt[end],digits=3))
+        verbose && sim_info(sim)
     end
 end
 function sim_step!(sim::AbstractSimulation;remeasure=true,udf=nothing,kwargs...)
@@ -119,7 +118,13 @@ function measure!(sim::AbstractSimulation,t=sum(sim.flow.Δt))
     update!(sim.pois)
 end
 
-export AbstractSimulation,Simulation,sim_step!,sim_time,measure!
+"""
+    sim_info(sim::AbstractSimulation)
+Prints information on the current state of a simulation.
+"""
+sim_info(sim::AbstractSimulation) = println("tU/L=",round(sim_time(sim),digits=4),", Δt=",round(sim.flow.Δt[end],digits=3))
+
+export AbstractSimulation,Simulation,sim_step!,sim_time,measure!,sim_info
 
 # default WriteVTK functions
 function vtkWriter end
@@ -134,7 +139,7 @@ function restart_sim! end
 # export
 export restart_sim!
 
-#default Plots functions
+# default Plots functions
 function flood end
 function addbody end
 function body_plot! end
@@ -142,6 +147,11 @@ function sim_gif! end
 function plot_logger end
 # export
 export flood,addbody,body_plot!,sim_gif!,plot_logger
+
+# default GLMakie functions
+function viz! end
+# export
+export viz!
 
 # Check number of threads when loading WaterLily
 """
@@ -166,6 +176,7 @@ function __init__()
         @require WriteVTK = "64499a7a-5c06-52f2-abe2-ccb03c286192" include("../ext/WaterLilyWriteVTKExt.jl")
         @require ReadVTK = "dc215faf-f008-4882-a9f7-a79a826fadc3" include("../ext/WaterLilyReadVTKExt.jl")
         @require Plots = "91a5bcdd-55d7-5caf-9e0b-520d859cae80" include("../ext/WaterLilyPlotsExt.jl")
+        @require GLMakie = "e9467ef8-e4e7-5192-8a1a-b1aee30e663a" include("../ext/WaterLilyGLMakieExt.jl")
     end
     check_nthreads(Val{Threads.nthreads()}())
 end
