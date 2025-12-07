bb100b6c676200810c88a42f3c4bb3bb050f2b82
weymouth <gabriel.weymouth@gmail.com>
2025-12-07 16:00:42 +0100

    Forgot to update docstring

--- src/AutoBody.jl
+++ src/AutoBody.jl
@@ -1,14 +1,11 @@
 """
-    AutoBody(sdf,map=(x,t)->x; compose=true) <: AbstractBody
+    AutoBody(sdf,map=(x,t)->x) <: AbstractBody
 
   - `sdf(x::AbstractVector,t::Real)::Real`: signed distance function
   - `map(x::AbstractVector,t::Real)::AbstractVector`: coordinate mapping function
-  - `compose::Bool=true`: Flag for composing `sdf=sdf∘map`
 
 Implicitly define a geometry by its `sdf` and optional coordinate `map`. Note: the `map`
-is composed automatically if `compose=true`, i.e. `sdf(x,t) = sdf(map(x,t),t)`.
-Both parameters remain independent otherwise. It can be particularly helpful to set
-`compose=false` when adding multiple bodies together to create a more complex one.
+is composed automatically i.e. `sdf(body::AutoBody,x,t) = body.sdf(body.map(x,t),t)`.
 """
 struct AutoBody{F1<:Function,F2<:Function} <: AbstractBody
     sdf::F1
